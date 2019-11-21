61dd552cb180b209c1136847eb238e0d9614a75c
Eric Niebler <eniebler@boost.org>
2019-11-21 12:44:40 -0800

    Use GoogleTest test harness (#43)
    Add cmake magic to download and build gtest; port any_unique_test to use gtest; refs #39

---
+++ cmake/gtest.cmake
@@ -0,0 +1,33 @@
+# Copyright (c) 2019-present, Facebook, Inc.
+#
+# This source code is licensed under the Apache License found in the
+# LICENSE.txt file in the root directory of this source tree.
+
+# Download and unpack googletest at configure time
+configure_file(cmake/CMakeLists.txt.in googletest-download/CMakeLists.txt)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
+
+if(result)
+  message(FATAL_ERROR "CMake step for googletest failed: ${result}")
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
+
+if(result)
+  message(FATAL_ERROR "Build step for googletest failed: ${result}")
+endif()
+
+# Prevent overriding the parent project's compiler/linker
+# settings on Windows
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+# Add googletest directly to our build. This defines
+# the gtest and gtest_main targets.
+add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/googletest-src
+                 ${CMAKE_CURRENT_BINARY_DIR}/googletest-build
+                 EXCLUDE_FROM_ALL)
---
+++ test/any_unique_test.cpp
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <unifex/any_unique.hpp>
+#include <unifex/type_traits.hpp>
+#include <unifex/memory_resource.hpp>
+
+#include <cassert>
+#include <string>
+#include <typeindex>
+#include <atomic>
+
+#include <gtest/gtest.h>
+
+template <typename T>
+using is_type_index = std::is_same<std::type_index, T>;
+
+inline constexpr struct get_typeid_cpo {
+  using type_erased_signature_t =
+      std::type_index(const unifex::this_&) noexcept;
+
+  template <typename T>
+  friend std::type_index tag_invoke(get_typeid_cpo, const T& x) {
+    return typeid(x);
+  }
+
+  template <typename T>
+  auto operator()(const T& x) const noexcept ->
+      unifex::tag_invoke_result_t<get_typeid_cpo, const T&> {
+    static_assert(
+      std::is_same_v<std::type_index, unifex::tag_invoke_result_t<get_typeid_cpo, const T&>>);
+    return tag_invoke(get_typeid_cpo{}, x);
+  }
+} get_typeid;
+
+struct destructor {
+  explicit destructor(bool& x) : ref_(x) {}
+  ~destructor() {
+    ref_ = true;
+  }
+  bool& ref_;
+};
+
+#if !UNIFEX_NO_MEMORY_RESOURCE
+using namespace unifex::pmr;
+
+class counting_memory_resource : public memory_resource {
+ public:
+  explicit counting_memory_resource(memory_resource* r) noexcept : inner_(r) {}
+
+  std::size_t total_allocated_bytes() const {
+    return allocated_.load();
+  }
+
+ private:
+  void* do_allocate(std::size_t bytes, std::size_t alignment) override {
+    void* p = inner_->allocate(bytes, alignment);
+    allocated_ += bytes;
+    return p;
+  }
+
+  void do_deallocate(void* p, std::size_t bytes, std::size_t alignment)
+      override {
+    allocated_ -= bytes;
+    inner_->deallocate(p, bytes, alignment);
+  }
+
+  bool do_is_equal(const memory_resource& other) const noexcept override {
+    return &other == this;
+  }
+
+  memory_resource* inner_;
+  std::atomic<std::size_t> allocated_ = 0;
+};
+#endif
+
+using A = unifex::any_unique_t<get_typeid>;
+using B = unifex::any_unique_t<>;
+
+TEST(AnyUniqueTest, WithTypeid) {
+  const ::A a = std::string{"hello"};
+  auto id = get_typeid(a);
+  EXPECT_EQ(id, typeid(std::string));
+}
+
+TEST(AnyUniqueTest, WithoutTypeid) {
+  const ::B b = std::string{"hello"};
+  auto id = get_typeid(b);
+  EXPECT_EQ(id, typeid(B));
+}
+
+TEST(AnyUniqueTest, TestDestructor) {
+  bool hasDestructorRun = false;
+  {
+    const A a{std::in_place_type<destructor>, hasDestructorRun};
+    EXPECT_EQ(get_typeid(a), typeid(destructor));
+    EXPECT_FALSE(hasDestructorRun);
+  }
+  EXPECT_TRUE(hasDestructorRun);
+}
+
+#if !UNIFEX_NO_MEMORY_RESOURCE
+TEST(AnyUniqueTest, WithCustomAllocator) {
+  counting_memory_resource res{new_delete_resource()};
+  polymorphic_allocator<char> alloc{&res};
+  {
+    A a1{std::string("hello"), alloc};
+    EXPECT_GE(res.total_allocated_bytes(), sizeof(std::string));
+    A a2{std::allocator_arg, alloc, std::in_place_type<std::string>, "hello"};
+    EXPECT_GE(res.total_allocated_bytes(), 2 * sizeof(std::string));
+  }
+  EXPECT_EQ(res.total_allocated_bytes(), 0);
+}
+#endif
