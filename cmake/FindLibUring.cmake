bde8953d9ded01cedb32d402064c5a34cdc157b2
Lewis Baker <lbaker@fb.com>
2020-02-07 15:36:37 -0800

    No longer link with liburing.a.
    We now only depend on its liburin/io_uring.h header.
    When detecting liburing support now only check for this header.

--- cmake/FindLibUring.cmake
+++ cmake/FindLibUring.cmake
@@ -12,18 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-find_path(LIBURING_INCLUDE_DIR NAMES liburing.h)
+find_path(LIBURING_INCLUDE_DIR NAMES liburing/io_uring.h)
 mark_as_advanced(LIBURING_INCLUDE_DIR)
 
-find_library(LIBURING_LIBRARY NAMES uring)
-mark_as_advanced(LIBURING_LIBRARY)
+#find_library(LIBURING_LIBRARY NAMES uring)
+#mark_as_advanced(LIBURING_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(
         LIBURING
-        REQUIRED_VARS LIBURING_LIBRARY LIBURING_INCLUDE_DIR)
+        REQUIRED_VARS LIBURING_INCLUDE_DIR)
 
 if(LIBURING_FOUND)
-  set(LIBURING_LIBRARIES ${LIBURING_LIBRARY})
+  #set(LIBURING_LIBRARIES ${LIBURING_LIBRARY})
   set(LIBURING_INCLUDE_DIRS ${LIBURING_INCLUDE_DIR})
 endif()
