74e5ced40dd5aac66ebaeee8861d493a1f92bad1
Lewis Baker <lbaker@fb.com>
2020-03-04 00:09:30 -0800

    Modify transform() to use better ADL insulation techniques.

--- include/unifex/transform.hpp
+++ include/unifex/transform.hpp
@@ -26,155 +26,257 @@
 
 #include <functional>
 #include <type_traits>
+#include <utility>
 
 namespace unifex {
 
 namespace detail
 {
-  template <typename Result, typename = void>
+  template<typename Func>
   struct transform_result_overload {
-    using type = type_list<Result>;
+  private:
+    template<typename Result>
+    struct impl {
+      using type = type_list<Result>;
+    };
+
+    template<>
+    struct impl<void> {
+      using type = type_list<>;
+    };
+  public:
+    template<typename... Args>
+    using apply = type_list<typename impl<std::invoke_result_t<Func, Args...>>::type>;
   };
-  template <typename Result>
-  struct transform_result_overload<Result, std::enable_if_t<std::is_void_v<Result>>> {
-    using type = type_list<>;
+
+  template<typename Predecessor, typename Func, typename Receiver>
+  struct transform_operation {
+    class type;
   };
-}
 
-template <typename Predecessor, typename Func>
-struct transform_sender {
-  UNIFEX_NO_UNIQUE_ADDRESS Predecessor pred_;
-  UNIFEX_NO_UNIQUE_ADDRESS Func func_;
-
-private:
-
-  // This helper transforms an argument list into either
-  // - type_list<type_list<Result>> - if Result is non-void, or
-  // - type_list<type_list<>>       - if Result is void
-  template<typename... Args>
-  using transform_result = type_list<
-    typename detail::transform_result_overload<
-      std::invoke_result_t<Func, Args...>>::type>;
-
-public:
-
-  template <
-      template <typename...> class Variant,
-      template <typename...> class Tuple>
-  using value_types = type_list_nested_apply_t<
-    typename Predecessor::template value_types<concat_type_lists_unique_t, transform_result>,
-    Variant,
-    Tuple>;
-
-  template <template <typename...> class Variant>
-  using error_types = typename concat_type_lists_unique_t<
-    typename Predecessor::template error_types<type_list>,
-    type_list<std::exception_ptr>>::template apply<Variant>;
-
-  friend constexpr auto tag_invoke(
-      tag_t<blocking>,
-      const transform_sender& sender) {
-    return blocking(sender.pred_);
-  }
-
-  template <typename Receiver>
+  template<typename Predecessor, typename Func, typename Receiver>
   struct transform_receiver {
-    UNIFEX_NO_UNIQUE_ADDRESS Func func_;
-    UNIFEX_NO_UNIQUE_ADDRESS Receiver receiver_;
+    class type {
+      using operation_state = typename transform_operation<Predecessor, Func, Receiver>::type;
+    public:
+      explicit type(operation_state* op) noexcept
+      : op_(op)
+      {}
 
-    template <typename... Values>
-    void set_value(Values&&... values) && noexcept {
-      using result_type = std::invoke_result_t<Func, Values...>;
-      if constexpr (std::is_void_v<result_type>) {
-        if constexpr (noexcept(std::invoke(
-                          (Func &&) func_, (Values &&) values...))) {
-          std::invoke((Func &&) func_, (Values &&) values...);
-          unifex::set_value((Receiver &&) receiver_);
-        } else {
-          try {
-            std::invoke((Func &&) func_, (Values &&) values...);
-            unifex::set_value((Receiver &&) receiver_);
-          } catch (...) {
-            unifex::set_error((Receiver &&) receiver_, std::current_exception());
+      type(type&& other) noexcept
+      : op_(std::exchange(other.op_, nullptr))
+      {}
+
+      template <typename... Values>
+      void set_value(Values&&... values) && noexcept {
+        using result_type = std::invoke_result_t<Func, Values...>;
+        if constexpr (std::is_void_v<result_type>) {
+          if constexpr (std::is_nothrow_invocable_v<Func, Values...> &&
+                        std::is_nothrow_invocable_v<decltype(unifex::set_value), Receiver>) {
+            std::invoke((Func &&) op_->func_, (Values &&) values...);
+            unifex::set_value((Receiver &&) op_->receiver_);
+          } else {
+            try {
+              std::invoke((Func &&) op_->func_, (Values &&) values...);
+              unifex::set_value((Receiver &&) op_->receiver_);
+            } catch (...) {
+              unifex::set_error((Receiver &&) op_->receiver_, std::current_exception());
+            }
           }
-        }
-      } else {
-        if constexpr (noexcept(std::invoke(
-                          (Func &&) func_, (Values &&) values...))) {
-          unifex::set_value(
-              (Receiver &&) receiver_,
-              std::invoke((Func &&) func_, (Values &&) values...));
         } else {
-          try {
+          if constexpr (std::is_nothrow_invocable_v<Func, Values...> &&
+                        std::is_nothrow_invocable_v<decltype(unifex::set_value), result_type>) {
             unifex::set_value(
-                (Receiver &&) receiver_,
-                std::invoke((Func &&) func_, (Values &&) values...));
-          } catch (...) {
-            unifex::set_error((Receiver &&) receiver_, std::current_exception());
+              (Receiver &&) op_->receiver_,
+              std::invoke((Func &&) op_->func_, (Values &&) values...));
+          } else {
+            try {
+              unifex::set_value(
+                (Receiver &&) op_->receiver_,
+                std::invoke((Func &&) op_->func_, (Values &&) values...));
+            } catch (...) {
+              unifex::set_error((Receiver &&) op_->receiver_, std::current_exception());
+            }
           }
         }
       }
-    }
 
-    template <typename Error>
-    void set_error(Error&& error) && noexcept {
-      unifex::set_error((Receiver &&) receiver_, (Error &&) error);
-    }
+      template <typename Error>
+      void set_error(Error&& error) && noexcept {
+        unifex::set_error((Receiver &&) op_->receiver_, (Error &&) error);
+      }
 
-    void set_done() && noexcept {
-      unifex::set_done((Receiver &&) receiver_);
-    }
+      void set_done() && noexcept {
+        unifex::set_done((Receiver &&) op_->receiver_);
+      }
 
-    template <
+    private:
+      template <
         typename CPO,
         std::enable_if_t<!is_receiver_cpo_v<CPO>, int> = 0>
-    friend auto tag_invoke(CPO cpo, const transform_receiver& r) noexcept(
-        std::is_nothrow_invocable_v<CPO, const Receiver&>)
-        -> std::invoke_result_t<CPO, const Receiver&> {
-      return std::move(cpo)(std::as_const(r.receiver_));
-    }
+      friend auto tag_invoke(CPO cpo, const type& r) noexcept(
+          std::is_nothrow_invocable_v<CPO, const Receiver&>)
+          -> std::invoke_result_t<CPO, const Receiver&> {
+        return std::move(cpo)(r.get_const_receiver());
+      }
 
-    template <typename Visit>
-    friend void tag_invoke(
-        tag_t<visit_continuations>,
-        const transform_receiver& r,
-        Visit&& visit) {
-      std::invoke(visit, r.receiver_);
-    }
+      template <typename Visit>
+      friend void tag_invoke(
+          tag_t<visit_continuations>,
+          const type& r,
+          Visit&& visit) {
+        std::invoke(visit, r.get_const_receiver());
+      }
+
+      const Receiver& get_const_receiver() const noexcept {
+        return op_->receiver_;
+      }
+
+      operation_state* op_;
+    };
   };
 
-  template <typename Receiver>
-  auto connect(Receiver&& receiver) &&
-      -> operation_t<Predecessor, transform_receiver<std::remove_cvref_t<Receiver>>> {
-    return unifex::connect(
-        std::forward<Predecessor>(pred_),
-        transform_receiver<std::remove_cvref_t<Receiver>>{
-            std::forward<Func>(func_), std::forward<Receiver>(receiver)});
+  template<typename Predecessor, typename Func, typename Receiver>
+  class transform_operation<Predecessor, Func, Receiver>::type {
+    using receiver_wrapper = typename transform_receiver<Predecessor, Func, Receiver>::type;
+
+  public:
+    template<typename Func2, typename Receiver2>
+    explicit type(Predecessor&& pred, Func2&& func, Receiver2&& receiver)
+    : func_((Func2&&)func)
+    , receiver_((Receiver2&&)receiver)
+    , op_(unifex::connect((Predecessor&&)pred, receiver_wrapper{this}))
+    {}
+
+    void start() & noexcept {
+      unifex::start(op_);
+    }
+
+  private:
+    friend class transform_receiver<Predecessor, Func, Receiver>::type;
+
+    UNIFEX_NO_UNIQUE_ADDRESS Func func_;
+    UNIFEX_NO_UNIQUE_ADDRESS Receiver receiver_;
+    operation_t<Predecessor, typename transform_receiver<Predecessor, Func, Receiver>::type> op_;
+  };
+
+  template<typename Predecessor, typename Func>
+  struct transform_sender {
+    class type {
+    public:
+      template <
+          template <typename...> class Variant,
+          template <typename...> class Tuple>
+      using value_types = type_list_nested_apply_t<
+        typename Predecessor::template value_types<
+          concat_type_lists_unique_t,
+          transform_result_overload<Func>::template apply>,
+        Variant,
+        Tuple>;
+
+      template <template <typename...> class Variant>
+      using error_types = typename concat_type_lists_unique_t<
+        typename Predecessor::template error_types<type_list>,
+        type_list<std::exception_ptr>>::template apply<Variant>;
+
+      template<typename Predecessor2, typename Func2>
+      explicit type(Predecessor2&& pred, Func2&& func)
+      : pred_((Predecessor2&&)pred)
+      , func_((Func2&&)func)
+      {}
+
+    private:
+
+      template <
+        typename Receiver,
+        std::enable_if_t<
+          is_connectable_v<
+            Predecessor,
+            typename transform_receiver<Predecessor, Func, std::decay_t<Receiver>>::type>, int> = 0>
+      friend auto tag_invoke(tag_t<connect>, type&& s, Receiver&& receiver)
+          noexcept(std::is_nothrow_move_constructible_v<Func> &&
+                   std::is_nothrow_constructible_v<std::decay_t<Receiver>, Receiver> &&
+                   is_nothrow_connectable_v<Predecessor, typename transform_receiver<Predecessor, Func, std::decay_t<Receiver>>::type>)
+          -> typename transform_operation<Predecessor, Func, std::decay_t<Receiver>>::type {
+        return typename transform_operation<Predecessor, Func, std::decay_t<Receiver>>::type{
+          (Predecessor&&)s.pred_,
+          (Func&&)s.func_,
+          (Receiver&&)receiver
+        };
+      }
+
+
+      template <
+        typename Receiver,
+        std::enable_if_t<
+          is_connectable_v<
+            Predecessor&,
+            typename transform_receiver<Predecessor&, Func, std::decay_t<Receiver>>::type>, int> = 0>
+      friend auto tag_invoke(tag_t<connect>, type& s, Receiver&& receiver)
+          noexcept(std::is_nothrow_constructible_v<Func, Func&> &&
+                   std::is_nothrow_constructible_v<std::decay_t<Receiver>, Receiver> &&
+                   is_nothrow_connectable_v<Predecessor&, typename transform_receiver<Predecessor&, Func, std::decay_t<Receiver>>::type>)
+          -> typename transform_operation<Predecessor&, Func, std::decay_t<Receiver>>::type {
+        return typename transform_operation<Predecessor&, Func, std::decay_t<Receiver>>::type{
+          s.pred_,
+          s.func_,
+          (Receiver&&)receiver
+        };
+      }
+
+      template <
+        typename Receiver,
+        std::enable_if_t<
+          is_connectable_v<
+            const Predecessor&,
+            typename transform_receiver<const Predecessor&, Func, std::decay_t<Receiver>>::type>, int> = 0>
+      friend auto tag_invoke(tag_t<connect>, const type& s, Receiver&& receiver)
+          noexcept(std::is_nothrow_copy_constructible_v<Func> &&
+                   std::is_nothrow_constructible_v<std::decay_t<Receiver>, Receiver> &&
+                   is_nothrow_connectable_v<const Predecessor&, typename transform_receiver<const Predecessor&, Func, std::decay_t<Receiver>>::type>)
+          -> typename transform_operation<const Predecessor&, Func, std::decay_t<Receiver>>::type {
+        return typename transform_operation<const Predecessor&, Func, std::decay_t<Receiver>>::type{
+          std::as_const(s.pred_),
+          std::as_const(s.func_),
+          (Receiver&&)receiver
+        };
+      }
+
+      friend constexpr auto tag_invoke(
+          tag_t<blocking>,
+          const type& sender) {
+        return blocking(sender.pred_);
+      }
+
+      UNIFEX_NO_UNIQUE_ADDRESS Predecessor pred_;
+      UNIFEX_NO_UNIQUE_ADDRESS Func func_;
+    };
+  };
+} // namespace detail
+
+inline constexpr struct transform_cpo {
+  template<
+    typename Sender,
+    typename Func,
+    std::enable_if_t<is_tag_invocable_v<transform_cpo, Sender, Func>, int> = 0>
+  auto operator()(Sender&& sender, Func&& func) const
+    noexcept(is_nothrow_tag_invocable_v<transform_cpo, Sender, Func>)
+    -> tag_invoke_result_t<transform_cpo, Sender, Func> {
+    return unifex::tag_invoke(*this, (Sender&&)sender, (Func&&)func);
   }
 
-  template <typename Receiver>
-  auto connect(Receiver&& receiver) &
-      -> operation_t<Predecessor&, transform_receiver<std::remove_cvref_t<Receiver>>>{
-    return unifex::connect(
-        pred_,
-        transform_receiver<std::remove_cvref_t<Receiver>>{
-            func_, std::forward<Receiver>(receiver)});
+  template<
+    typename Sender,
+    typename Func,
+    std::enable_if_t<
+      !is_tag_invocable_v<transform_cpo, Sender, Func>, int> = 0>
+  auto operator()(Sender&& sender, Func&& func) const
+    noexcept(std::is_nothrow_constructible_v<std::decay_t<Sender>, Sender> &&
+             std::is_nothrow_constructible_v<std::decay_t<Func>, Func>)
+    -> typename detail::transform_sender<std::decay_t<Sender>, std::decay_t<Func>>::type {
+    return typename detail::transform_sender<std::decay_t<Sender>, std::decay_t<Func>>::type(
+      (Sender&&)sender, (Func&&)func);
   }
-
-  template <typename Receiver>
-  auto connect(Receiver&& receiver) const &
-      -> operation_t<const Predecessor&, transform_receiver<std::remove_cvref_t<Receiver>>> {
-    return unifex::connect(
-        pred_,
-        transform_receiver<std::remove_cvref_t<Receiver>>{
-            func_, std::forward<Receiver>(receiver)});
-  }
-};
-
-template <typename Sender, typename Func>
-auto transform(Sender&& predecessor, Func&& func) {
-  return transform_sender<std::remove_cvref_t<Sender>, std::decay_t<Func>>{
-      (Sender &&) predecessor, (Func &&) func};
-}
+} transform;
 
 } // namespace unifex
