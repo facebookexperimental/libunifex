facebookexperimental/libunifex

commit 8d1ec382fbeeaca7c884c8bdb248bce5f3db5c8a

Add connect overloads for const-lvalue-ref (#95)
The `connect()` overloads taking const-lvalue-ref are required by some algorithms
that need to repeat operations (e.g. `retry_when()`) require being able to call
`connect()` on the same sender multiple times, which is not permitted if passing
an rvalue.

This change rounds out the set of algorithms that support lvalue `connect()`

Authored-by: Kirk Shoop <kirkshoop@fb.com>

include/unifex/transform.hpp
@@ -161,6 +161,10 @@ public:
 
   template <typename Receiver>
   auto connect(Receiver&& r) &&
+      noexcept(
+        std::is_nothrow_constructible_v<std::remove_cvref_t<Receiver>, Receiver> && 
+        std::is_nothrow_move_constructible_v<Func> &&
+        is_nothrow_connectable_v<Predecessor, receiver<std::remove_cvref_t<Receiver>>>)
       -> operation_t<Predecessor, receiver<std::remove_cvref_t<Receiver>>> {
     return unifex::connect(
         std::forward<Predecessor>(pred_),
@@ -169,15 +173,11 @@ public:
   }
 
   template <typename Receiver>
-  auto connect(Receiver&& r) &
-      -> operation_t<Predecessor&, receiver<std::remove_cvref_t<Receiver>>>{
-    return unifex::connect(
-        pred_,
-        receiver<std::remove_cvref_t<Receiver>>{func_, std::forward<Receiver>(r)});
-  }
-
-  template <typename Receiver>
   auto connect(Receiver&& r) const &
+      noexcept(
+        std::is_nothrow_constructible_v<std::remove_cvref_t<Receiver>, Receiver> && 
+        std::is_nothrow_copy_constructible_v<Func> &&
+        is_nothrow_connectable_v<const Predecessor&, receiver<std::remove_cvref_t<Receiver>>>)
       -> operation_t<const Predecessor&, receiver<std::remove_cvref_t<Receiver>>> {
     return unifex::connect(
         pred_,

include/unifex/when_all.hpp
@@ -253,8 +253,8 @@ struct _op<Receiver, Senders...>::type {
     }
   }
 
-  std::tuple<std::optional<value_variant_for_sender<Senders>>...> values_;
-  std::optional<error_types<std::variant, Senders...>> error_;
+  std::tuple<std::optional<value_variant_for_sender<std::remove_cvref_t<Senders>>>...> values_;
+  std::optional<error_types<std::variant, std::remove_cvref_t<Senders>...>> error_;
   std::atomic<std::size_t> refCount_{sizeof...(Senders)};
   std::atomic<bool> doneOrError_{false};
   inplace_stop_source stopSource_;
@@ -298,6 +298,13 @@ class _sender<Senders...>::type {
       return operation<Receiver, Senders...>{
           (Receiver &&) receiver, (Senders &&) senders...};
     }, std::move(senders_));
+  }
+  template <typename Receiver>
+  operation<Receiver, const Senders&...> connect(Receiver&& receiver) const & {
+    return std::apply([&](const Senders&... senders) {
+      return operation<Receiver, const Senders&...>{
+          (Receiver &&) receiver, senders...};
+    }, senders_);
   }
 
  private:

CMakeLists.txt
@@ -22,6 +22,9 @@ if(BUILD_TESTING OR UNIFEX_BUILD_EXAMPLES)
   include(gtest)
   include(CTest)
 
+  target_compile_features(gtest PUBLIC cxx_std_20)
+  target_compile_features(gtest_main PUBLIC cxx_std_20)
+
   add_subdirectory(examples)
   if(BUILD_TESTING)
     add_subdirectory(test)

cmake/unifex_env.cmake
@@ -18,9 +18,3 @@ elseif("x${CMAKE_CXX_COMPILER_ID}" STREQUAL "xMSVC")
 else()
   message(WARNING "[unifex warning]: unknown compiler ${CMAKE_CXX_COMPILER_ID} !")
 endif()
-
-if(UNIFEX_CXX_COMPILER_MSVC OR UNIFEX_CXX_COMPILER_CLANGCL)
-  set(UNIFEX_CXX_STD "/std:latest")
-else()
-  set(UNIFEX_CXX_STD "-std=gnu++2a")
-endif()

examples/async_trace.cpp
@@ -21,6 +21,7 @@
 
 #include <unifex/config.hpp>
 #include <unifex/just.hpp>
+#include <unifex/let.hpp>
 #include <unifex/timed_single_thread_context.hpp>
 #include <unifex/transform.hpp>
 #include <unifex/finally.hpp>
@@ -41,6 +42,11 @@
 using namespace unifex;
 using namespace std::chrono;
 using namespace std::chrono_literals;
+
+template <typename F>
+auto defer(F&& f) {
+  return let(just(), (F&&)f);
+}
 
 auto dump_async_trace(std::string tag = {}) {
   return transform(
@@ -74,26 +80,28 @@ int main() {
 
   auto start = steady_clock::now();
 
-  sync_wait(transform(
-      when_all(
+  sync_wait(
+    transform(
+      defer([&]{
+        return when_all(
           transform(
-              dump_async_trace_on_start(
-                  schedule_after(context.get_scheduler(), 100ms), "part1"),
-              [=]() {
-                auto time = steady_clock::now() - start;
-                auto timeMs = duration_cast<milliseconds>(time).count();
-                std::cout << "part1 finished - [" << timeMs << "]\n";
-                return time;
-              }),
+            dump_async_trace_on_start(
+              schedule_after(context.get_scheduler(), 100ms), "part1"),
+            [=]() {
+              auto time = steady_clock::now() - start;
+              auto timeMs = duration_cast<milliseconds>(time).count();
+              std::cout << "part1 finished - [" << timeMs << "]\n";
+              return time;
+            }),
           transform(
-              dump_async_trace_on_completion(
-                  schedule_after(context.get_scheduler(), 200ms), "part2"),
-              [=]() {
-                auto time = steady_clock::now() - start;
-                auto timeMs = duration_cast<milliseconds>(time).count();
-                std::cout << "part2 finished - [" << timeMs << "]\n";
-                return time;
-              }),
+            dump_async_trace_on_completion(
+              schedule_after(context.get_scheduler(), 200ms), "part2"),
+            [=]() {
+              auto time = steady_clock::now() - start;
+              auto timeMs = duration_cast<milliseconds>(time).count();
+              std::cout << "part2 finished - [" << timeMs << "]\n";
+              return time;
+            }),
 #if !UNIFEX_NO_COROUTINES
           awaitable_sender(
             []() -> task<int> {
@@ -104,7 +112,8 @@ int main() {
 #else
           just(42)
 #endif // UNIFEX_NO_COROUTINES
-          ),
+        );
+      }),
       [](auto &&a, auto &&b, auto &&c) {
         std::cout
             << "when_all finished - ["

include/unifex/async_trace.hpp
@@ -199,7 +199,7 @@ namespace _async_trace {
     using error_types = Variant<std::exception_ptr>;
 
     template <typename Receiver>
-    operation<Receiver> connect(Receiver&& r) {
+    operation<Receiver> connect(Receiver&& r) const& {
       return operation<Receiver>{(Receiver &&) r};
     }
 

include/unifex/dematerialize.hpp
@@ -193,20 +193,6 @@ namespace _demat {
     template <
         typename Receiver,
         std::enable_if_t<
-            is_connectable_v<Source&, receiver<Receiver>>,
-            int> = 0>
-    auto connect(Receiver&& r) &
-        noexcept(is_nothrow_connectable_v<Source&, receiver<Receiver>> &&
-                 std::is_nothrow_constructible_v<std::remove_cvref_t<Receiver>, Receiver>)
-        -> operation_t<Source&, receiver<Receiver>> {
-      return unifex::connect(
-          source_,
-          receiver<Receiver>{static_cast<Receiver&&>(r)});
-    }
-
-    template <
-        typename Receiver,
-        std::enable_if_t<
             is_connectable_v<const Source&, receiver<Receiver>>,
             int> = 0>
     auto connect(Receiver&& r) const &

include/unifex/finally.hpp
@@ -617,19 +617,19 @@ namespace unifex
       }
 
     private:
-      UNIFEX_NO_UNIQUE_ADDRESS CompletionSender completionSender_;
+      UNIFEX_NO_UNIQUE_ADDRESS std::remove_cvref_t<CompletionSender> completionSender_;
       UNIFEX_NO_UNIQUE_ADDRESS Receiver receiver_;
       bool started_ = false;
 
       // Result storage.
       union {
-        // Storage for error-types that might be produced by ValueSender.
+        // Storage for error-types that might be produced by SourceSender.
         UNIFEX_NO_UNIQUE_ADDRESS
-        typename SourceSender::template error_types<error_result_union>
+        typename std::remove_cvref_t<SourceSender>::template error_types<error_result_union>
             error_;
 
-        // Storage for value-types that might be produced by ValueSender.
-        UNIFEX_NO_UNIQUE_ADDRESS typename SourceSender::template value_types<
+        // Storage for value-types that might be produced by SourceSender.
+        UNIFEX_NO_UNIQUE_ADDRESS typename std::remove_cvref_t<SourceSender>::template value_types<
             manual_lifetime_union,
             decayed_tuple<std::tuple>::template apply>
             value_;
@@ -645,13 +645,13 @@ namespace unifex
 
         // Storage for the completion operation for the case where
         // the source operation completed with a value.
-        typename SourceSender::
+        typename std::remove_cvref_t<SourceSender>::
             template value_types<manual_lifetime_union, value_operation>
                 completionValueOp_;
 
         // Storage for the completion operation for the case where the
         // source operation completed with an error.
-        typename SourceSender::template error_types<error_operation_union>
+        typename std::remove_cvref_t<SourceSender>::template error_types<error_operation_union>
             completionErrorOp_;
 
         // Storage for the completion operation for the case where the
@@ -700,40 +700,73 @@ namespace unifex
         : source_(static_cast<SourceSender2&&>(source))
         , completion_(static_cast<CompletionSender2&&>(completion)) {}
 
-      // TODO: Also constrain this method to check that the CompletionSender
+    private:
+
+      // TODO: Also constrain these methods to check that the CompletionSender
       // is connectable to any of the instantiations of done/value/error_receiver
       // that could be created for each of the results that SourceSender might
       // complete with. For now we just check done_receiver as an approximation.
+
       template <
-          typename Receiver,
-          typename CPO,
-          typename S,
-          std::enable_if_t<
-              std::conjunction_v<
-                std::is_same<CPO, tag_t<connect>>,
-                std::is_same<S, sender>,
-                is_connectable<
-                  SourceSender,
-                  receiver<
-                      SourceSender,
-                      CompletionSender,
-                      Receiver>>,
-                is_connectable<
-                  CompletionSender,
-                  done_receiver<
-                      SourceSender,
-                      CompletionSender,
-                      Receiver>>>,
-              int> = 0>
+        typename Receiver,
+        typename CPO,
+        typename S,
+        std::enable_if_t<
+          std::conjunction_v<
+            std::is_same<CPO, tag_t<connect>>,
+            std::is_same<S, sender>,
+            is_connectable<
+              SourceSender,
+              receiver<
+                SourceSender,
+                CompletionSender,
+                std::remove_cvref_t<Receiver>>>,
+            is_connectable<
+              CompletionSender,
+              done_receiver<
+                SourceSender,
+                CompletionSender,
+                std::remove_cvref_t<Receiver>>>>,
+            int(*)[__LINE__]> = nullptr>
       friend auto tag_invoke(CPO, S&& s, Receiver&& r)
           -> operation<SourceSender, CompletionSender, Receiver> {
         return operation<SourceSender, CompletionSender, Receiver>{
-                static_cast<SourceSender&&>(s.source_),
-                static_cast<CompletionSender&&>(s.completion_),
+                static_cast<S&&>(s).source_,
+                static_cast<S&&>(s).completion_,
                 static_cast<Receiver&&>(r)};
       }
 
-    private:
+      template <
+        typename Receiver,
+        typename CPO,
+        typename S,
+        typename SourceSenderConstRef = const SourceSender&,
+        std::enable_if_t<
+          std::conjunction_v<
+            std::is_same<CPO, tag_t<connect>>,
+            std::is_same<std::remove_cvref_t<S>, sender>,
+            std::negation<std::is_same<S, sender>>,
+            is_connectable<
+              SourceSenderConstRef,
+              receiver<
+                SourceSenderConstRef,
+                CompletionSender,
+                std::remove_cvref_t<Receiver>>>,
+            is_connectable<
+              CompletionSender,
+              done_receiver<
+                SourceSenderConstRef,
+                CompletionSender,
+                std::remove_cvref_t<Receiver>>>>,
+            int(*)[__LINE__]> = nullptr>
+      friend auto tag_invoke(CPO, S&& s, Receiver&& r)
+          -> operation<SourceSenderConstRef, CompletionSender, Receiver> {
+        return operation<SourceSenderConstRef, CompletionSender, Receiver>{
+                static_cast<S&&>(s).source_,
+                static_cast<S&&>(s).completion_,
+                static_cast<Receiver&&>(r)};
+      }
+
       SourceSender source_;
       CompletionSender completion_;
     };

commit ecfd9ce626ddc37bce066ea4e0bdb61ef690e80a

Make transform/when_all connect() methods SFINAE-friendly.
Calling connect() on a transform/when_all sender was attempting to
instantiate both rvalue-ref and const-lvalue-ref overloads of connect().
If the predecessor was only rvalue-connectable then
this would result in a compile-error.

This change modifies the connect() methods to SFINAE out if that particular connect() overload is not supported.

This fixes compilation errors in io_uring_test that were
introduced in #95 which added some const-lvalue-ref
overloads of connect().

include/unifex/transform.hpp
@@ -159,7 +159,11 @@ public:
     return blocking(sender.pred_);
   }
 
-  template <typename Receiver>
+  template <
+    typename Receiver,
+    std::enable_if_t<
+      std::is_move_constructible_v<Func> &&
+      is_connectable_v<Predecessor, receiver<std::remove_cvref_t<Receiver>>>, int> = 0>
   auto connect(Receiver&& r) &&
       noexcept(
         std::is_nothrow_constructible_v<std::remove_cvref_t<Receiver>, Receiver> && 
@@ -172,7 +176,11 @@ public:
             std::forward<Func>(func_), std::forward<Receiver>(r)});
   }
 
-  template <typename Receiver>
+  template <
+    typename Receiver,
+    std::enable_if_t<
+      std::is_copy_constructible_v<Func> &&
+      is_connectable_v<const Predecessor&, receiver<std::remove_cvref_t<Receiver>>>, int> = 0>
   auto connect(Receiver&& r) const &
       noexcept(
         std::is_nothrow_constructible_v<std::remove_cvref_t<Receiver>, Receiver> && 

include/unifex/when_all.hpp
@@ -97,6 +97,13 @@ struct cancel_operation {
   }
 };
 
+template <typename Receiver, typename... Senders>
+struct _op {
+  struct type;
+};
+template <typename Receiver, typename... Senders>
+using operation = typename _op<std::remove_cvref_t<Receiver>, Senders...>::type;
+
 template <typename... Errors>
 using unique_decayed_error_types = concat_type_lists_unique_t<
   type_list<std::decay_t<Errors>>...>;
@@ -113,18 +120,18 @@ template <typename Sender>
 using value_variant_for_sender =
   typename Sender::template value_types<concat_type_lists_unique_t, decayed_value_tuple>::template apply<std::variant>;
 
-template <size_t Index, typename Operation>
+template <size_t Index, typename Receiver, typename... Senders>
 struct _element_receiver {
   struct type;
 };
-template <size_t Index, typename Operation>
-using element_receiver = typename _element_receiver<Index, Operation>::type;
+template <size_t Index, typename Receiver, typename... Senders>
+using element_receiver = typename _element_receiver<Index, Receiver, Senders...>::type;
 
-template <size_t Index, typename Operation>
-struct _element_receiver<Index, Operation>::type final {
+template <size_t Index, typename Receiver, typename... Senders>
+struct _element_receiver<Index, Receiver, Senders...>::type final {
   using element_receiver = type;
-  Operation& op_;
-  using receiver_type = typename Operation::receiver_type;
+
+  operation<Receiver, Senders...>& op_;
 
   template <typename... Values>
   void set_value(Values&&... values) noexcept {
@@ -155,7 +162,7 @@ struct _element_receiver<Index, Operation>::type final {
     op_.element_complete();
   }
 
-  receiver_type& get_receiver() const { return op_.receiver_; }
+  Receiver& get_receiver() const { return op_.receiver_; }
 
   template <
       typename CPO,
@@ -165,10 +172,10 @@ struct _element_receiver<Index, Operation>::type final {
         std::conjunction_v<
           std::negation<is_receiver_cpo<CPO>>,
           std::is_same<R, element_receiver>,
-          is_callable<CPO, const receiver_type&, Args...>>, int> = 0>
+          is_callable<CPO, const Receiver&, Args...>>, int> = 0>
   friend auto tag_invoke(CPO cpo, const R& r, Args&&... args) noexcept(
-      is_nothrow_callable_v<CPO, const receiver_type&, Args...>)
-      -> callable_result_t<CPO, const receiver_type&, Args...> {
+      is_nothrow_callable_v<CPO, const Receiver&, Args...>)
+      -> callable_result_t<CPO, const Receiver&, Args...> {
     return std::move(cpo)(std::as_const(r.get_receiver()), (Args&&)args...);
   }
 
@@ -192,17 +199,10 @@ struct _element_receiver<Index, Operation>::type final {
 };
 
 template <typename Receiver, typename... Senders>
-struct _op {
-  struct type;
-};
-template <typename Receiver, typename... Senders>
-using operation = typename _op<std::remove_cvref_t<Receiver>, Senders...>::type;
-
-template <typename Receiver, typename... Senders>
 struct _op<Receiver, Senders...>::type {
   using operation = type;
   using receiver_type = Receiver;
-  template<std::size_t Index, typename Operation>
+  template<std::size_t Index, typename Receiver2, typename... Senders2>
   friend class _element_receiver;
 
   explicit type(Receiver&& receiver, Senders&&... senders)
@@ -263,8 +263,8 @@ struct _op<Receiver, Senders...>::type {
       stopCallback_;
   Receiver receiver_;
   template<std::size_t Index>
-  using element_receiver = element_receiver<Index, operation>;
-  operation_tuple<0, element_receiver, Senders...> ops_;
+  using op_element_receiver = element_receiver<Index, Receiver, Senders...>;
+  operation_tuple<0, op_element_receiver, Senders...> ops_;
 };
 
 template <typename... Senders>
@@ -273,6 +273,13 @@ struct _sender {
 };
 template <typename... Senders>
 using sender = typename _sender<std::remove_cvref_t<Senders>...>::type;
+
+template<typename Receiver, typename Indices, typename... Senders>
+inline constexpr bool when_all_connectable_v = false;
+
+template<typename Receiver, std::size_t... Indices, typename... Senders>
+inline constexpr bool when_all_connectable_v<Receiver, std::index_sequence<Indices...>, Senders...> = std::conjunction_v<
+  is_connectable<Senders, element_receiver<Indices, Receiver, Senders...>>...>;  
 
 template <typename... Senders>
 class _sender<Senders...>::type {
@@ -292,14 +299,20 @@ class _sender<Senders...>::type {
   explicit type(Senders2&&... senders)
     : senders_((Senders2 &&) senders...) {}
 
-  template <typename Receiver>
+  template <
+    typename Receiver,
+    std::enable_if_t<
+      when_all_connectable_v<Receiver, std::index_sequence_for<Senders...>, Senders...>, int> = 0>
   operation<Receiver, Senders...> connect(Receiver&& receiver) && {
     return std::apply([&](Senders&&... senders) {
       return operation<Receiver, Senders...>{
           (Receiver &&) receiver, (Senders &&) senders...};
     }, std::move(senders_));
   }
-  template <typename Receiver>
+  template <
+    typename Receiver,
+    std::enable_if_t<
+      when_all_connectable_v<Receiver, std::index_sequence_for<Senders...>, const Senders&...>, int> = 0>
   operation<Receiver, const Senders&...> connect(Receiver&& receiver) const & {
     return std::apply([&](const Senders&... senders) {
       return operation<Receiver, const Senders&...>{