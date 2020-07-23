b50bcb5038230f63144b1f698f86345ee39a9196
LeeHowes <lwh@fb.com>
2020-07-23 15:58:25 -0700

    Find if (#141)
    Add a simple serial implementation of find_if over a pair of iterators.

---
+++ examples/find_if.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <unifex/just.hpp>
+#include <unifex/let.hpp>
+#include <unifex/scheduler_concepts.hpp>
+#include <unifex/sync_wait.hpp>
+#include <unifex/timed_single_thread_context.hpp>
+#include <unifex/find_if.hpp>
+#include <unifex/transform.hpp>
+#include <unifex/when_all.hpp>
+
+#include <chrono>
+#include <iostream>
+
+using namespace unifex;
+using namespace std::chrono;
+using namespace std::chrono_literals;
+
+int main() {
+  std::vector<int> input{1, 2, 3, 4};
+  // Apply linear find_if.
+  // As for std::find_if it returns the first instance that matches the
+  // predicate where the algorithm takes an iterator pair as the first
+  // two arguments, and forwards all other arguments to the predicate and
+  // onwards to the result.
+  // Precise API shape for the data being passed through is TBD, this is
+  // one option only.
+  std::optional<std::vector<int>::iterator> result = sync_wait(
+    transform(
+      find_if(
+          just(begin(input), end(input), 3),
+          [&](const int& v, int another_parameter) {
+            return v == another_parameter;
+          },
+          unifex::seq),
+      [](std::vector<int>::iterator v, int another_parameter) {
+        assert(another_parameter == 3);
+        return v;
+      }));
+
+  std::cout << "all done " << **result << "\n";
+
+  return 0;
+}
---
+++ include/unifex/find_if.hpp
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <unifex/config.hpp>
+#include <unifex/execution_policy.hpp>
+#include <unifex/receiver_concepts.hpp>
+#include <unifex/sender_concepts.hpp>
+#include <unifex/stream_concepts.hpp>
+#include <unifex/type_traits.hpp>
+#include <unifex/blocking.hpp>
+#include <unifex/get_stop_token.hpp>
+#include <unifex/async_trace.hpp>
+#include <unifex/type_list.hpp>
+#include <unifex/std_concepts.hpp>
+
+#include <exception>
+#include <functional>
+#include <type_traits>
+#include <utility>
+
+#include <unifex/detail/prologue.hpp>
+
+namespace unifex {
+namespace _find_if {
+
+template <typename Receiver, typename Func, typename FuncPolicy>
+struct _receiver {
+  struct type;
+};
+template <typename Receiver, typename Func, typename FuncPolicy>
+using receiver_t = typename _receiver<Receiver, Func, FuncPolicy>::type;
+
+template <typename Receiver, typename Func, typename FuncPolicy>
+struct _receiver<Receiver, Func, FuncPolicy>::type {
+  UNIFEX_NO_UNIQUE_ADDRESS Func func_;
+  UNIFEX_NO_UNIQUE_ADDRESS Receiver receiver_;
+  UNIFEX_NO_UNIQUE_ADDRESS FuncPolicy funcPolicy_;
+
+  template<typename Iterator, typename... Values>
+  auto find_if_helper(Iterator begin_it, Iterator end_it, const Values&... values) -> Iterator {
+    // Sequential implementation
+    for(auto it = begin_it; it != end_it; ++it) {
+      if(std::invoke((Func &&) func_, *it, (Values &&) values...)) {
+        return it;
+      }
+    }
+    return end_it;
+  }
+
+  template <typename BeginIt, typename EndIt, typename... Values>
+  void set_value(BeginIt begin_it, EndIt end_it, Values&&... values) && noexcept {
+    constexpr bool noexcept_func =
+      noexcept(std::invoke((Func &&) func_, *begin_it, (Values &&) values...));
+    constexpr bool noexcept_pp = noexcept(++begin_it);
+    constexpr bool noexcept_ne = noexcept(begin_it!=end_it);
+    if constexpr (noexcept_func && noexcept_pp && noexcept_ne) {
+      auto result = find_if_helper(begin_it, end_it, values...);
+
+      unifex::set_value((Receiver &&) receiver_, std::move(result), (Values &&) values...);
+    } else {
+      try {
+        auto result = find_if_helper(begin_it, end_it, values...);
+
+        unifex::set_value((Receiver &&) receiver_, std::move(result), (Values &&) values...);
+      } catch (...) {
+        unifex::set_error((Receiver &&) receiver_, std::current_exception());
+      }
+    }
+  }
+
+  template <typename Error>
+  void set_error(Error&& error) && noexcept {
+    unifex::set_error((Receiver &&) receiver_, (Error &&) error);
+  }
+
+  void set_done() && noexcept {
+    unifex::set_done((Receiver &&) receiver_);
+  }
+
+  template(typename CPO, typename R)
+      (requires is_receiver_query_cpo_v<CPO> AND same_as<R, type>)
+  friend auto tag_invoke(CPO cpo, const R& r) noexcept(
+      is_nothrow_callable_v<CPO, const Receiver&>)
+      -> callable_result_t<CPO, const Receiver&> {
+    return std::move(cpo)(std::as_const(r.receiver_));
+  }
+
+  template <typename Visit>
+  friend void tag_invoke(tag_t<visit_continuations>, const type& r, Visit&& visit) {
+    std::invoke(visit, r.receiver_);
+  }
+};
+
+template <typename Predecessor, typename Func, typename FuncPolicy>
+struct _sender {
+  struct type;
+};
+template <typename Predecessor, typename Func, typename FuncPolicy>
+using sender_t = typename _sender<Predecessor, Func, FuncPolicy>::type;
+
+template <typename Predecessor, typename Func, typename FuncPolicy>
+struct _sender<Predecessor, Func, FuncPolicy>::type {
+  UNIFEX_NO_UNIQUE_ADDRESS Predecessor pred_;
+  UNIFEX_NO_UNIQUE_ADDRESS Func func_;
+  UNIFEX_NO_UNIQUE_ADDRESS FuncPolicy funcPolicy_;
+
+private:
+
+  template <typename BeginIt, typename EndIt, typename... Args>
+  using result = type_list<type_list<BeginIt, Args...>>;
+
+public:
+
+  template <
+      template <typename...> class Variant,
+      template <typename...> class Tuple>
+  using value_types = type_list_nested_apply_t<
+    typename Predecessor::template value_types<concat_type_lists_unique_t, result>,
+    Variant,
+    Tuple>;
+
+  template <template <typename...> class Variant>
+  using error_types = typename concat_type_lists_unique_t<
+    typename Predecessor::template error_types<type_list>,
+    type_list<std::exception_ptr>>::template apply<Variant>;
+
+  template <typename Receiver>
+  using receiver_type = receiver_t<Receiver, Func, FuncPolicy>;
+
+  friend constexpr auto tag_invoke(tag_t<blocking>, const type& sender) {
+    return blocking(sender.pred_);
+  }
+
+  template(typename Sender, typename Receiver)
+    (requires same_as<remove_cvref_t<Sender>, type> AND receiver<Receiver>)
+  friend auto tag_invoke(tag_t<unifex::connect>, Sender&& s, Receiver&& r)
+    noexcept(
+      std::is_nothrow_constructible_v<remove_cvref_t<Receiver>, Receiver> &&
+      std::is_nothrow_constructible_v<Func, decltype((static_cast<Sender&&>(s).func_))> &&
+      is_nothrow_connectable_v<decltype((static_cast<Sender&&>(s).pred_)), receiver_type<remove_cvref_t<Receiver>>>)
+      -> connect_result_t<decltype((static_cast<Sender&&>(s).pred_)), receiver_type<remove_cvref_t<Receiver>>> {
+    return unifex::connect(
+      static_cast<Sender&&>(s).pred_,
+      receiver_type<remove_cvref_t<Receiver>>{
+        static_cast<Sender&&>(s).func_,
+        static_cast<Receiver&&>(r),
+        static_cast<Sender&&>(s).funcPolicy_});
+  }
+};
+} // namespace _find_if
+
+namespace _find_if_cpo {
+  inline const struct _fn {
+  public:
+    template(typename Sender, typename Func, typename FuncPolicy)
+      (requires tag_invocable<_fn, Sender, Func, FuncPolicy>)
+    auto operator()(Sender&& predecessor, Func&& func, FuncPolicy policy) const
+        noexcept(is_nothrow_tag_invocable_v<_fn, Sender, Func, FuncPolicy>)
+        -> tag_invoke_result_t<_fn, Sender, Func, FuncPolicy> {
+      return unifex::tag_invoke(_fn{}, (Sender&&)predecessor, (Func&&)func, (FuncPolicy&&)policy);
+    }
+    template(typename Sender, typename Func, typename FuncPolicy)
+      (requires (!tag_invocable<_fn, Sender, Func, FuncPolicy>))
+    auto operator()(Sender&& predecessor, Func&& func, FuncPolicy policy) const
+        noexcept(
+        std::is_nothrow_constructible_v<remove_cvref_t<Sender>, Sender> &&
+        std::is_nothrow_constructible_v<remove_cvref_t<Func>, Func> &&
+        std::is_nothrow_constructible_v<remove_cvref_t<FuncPolicy>, FuncPolicy>)
+        -> _find_if::sender_t<remove_cvref_t<Sender>, std::decay_t<Func>, FuncPolicy>{
+      return _find_if::sender_t<remove_cvref_t<Sender>, std::decay_t<Func>, FuncPolicy>{
+        (Sender &&) predecessor, (Func &&) func, (FuncPolicy &&) policy};
+    }
+  } find_if{};
+} // namespace _find_if_cpo
+using _find_if_cpo::find_if;
+} // namespace unifex
+
+#include <unifex/detail/epilogue.hpp>
