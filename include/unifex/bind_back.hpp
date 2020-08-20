8339a2da1b73036986a32237d85aac8f83b472d4
Kirk Shoop <kirk.shoop@gmail.com>
2020-08-20 12:42:16 -0700

    support apply_target composition

--- include/unifex/bind_back.hpp
+++ include/unifex/bind_back.hpp
@@ -26,6 +26,8 @@ namespace unifex {
 
 namespace _bind_back {
 
+struct instance_of_apply_target {};
+
 template <typename Cpo, typename... ArgN>
 struct _apply_target {
   struct type;
@@ -35,7 +37,7 @@ template <typename Cpo, typename... ArgN>
 using apply_target = typename _apply_target<Cpo, ArgN...>::type;
 
 template <typename Cpo, typename... ArgN>
-struct _apply_target<Cpo, ArgN...>::type {
+struct _apply_target<Cpo, ArgN...>::type : public instance_of_apply_target {
  private:
   std::tuple<remove_cvref_t<ArgN>...> argN_;
  public:
@@ -44,7 +46,18 @@ struct _apply_target<Cpo, ArgN...>::type {
     noexcept(std::is_nothrow_constructible_v<std::tuple<remove_cvref_t<ArgN>...>, CN...>) 
     : argN_((CN&&) cn...) {}
 
-  template <typename Target>
+  template (typename Target)
+    (requires (!std::is_base_of_v<instance_of_apply_target, remove_cvref_t<Target>>))
+  auto operator()(Target&& target) const &
+    noexcept(
+      is_nothrow_callable_v<Cpo, Target, ArgN...>) 
+    -> callable_result_t<Cpo, Target, ArgN...> {
+    return std::apply([&](auto&&... argN){
+      return Cpo{}((Target&&) target, argN...);
+    }, argN_);
+  }
+  template (typename Target)
+    (requires (!std::is_base_of_v<instance_of_apply_target, remove_cvref_t<Target>>))
   auto operator()(Target&& target) &&
     noexcept(
       is_nothrow_callable_v<Cpo, Target, ArgN...>) 
@@ -54,12 +67,51 @@ struct _apply_target<Cpo, ArgN...>::type {
     }, std::move(argN_));
   }
 
-  template <typename Target>
+  template (typename Target)
+    (requires (!std::is_base_of_v<instance_of_apply_target, remove_cvref_t<Target>>))
   friend auto operator|(Target&& target, type&& self) 
     noexcept(
       is_nothrow_callable_v<Cpo, Target, ArgN...>) 
     -> callable_result_t<Cpo, Target, ArgN...> {
     return std::move(self)((Target&&) target);
+  }
+  template (typename Target)
+    (requires (!std::is_base_of_v<instance_of_apply_target, remove_cvref_t<Target>>))
+  friend auto operator|(Target&& target, const type& self) 
+    noexcept(
+      is_nothrow_callable_v<Cpo, Target, ArgN...>) 
+    -> callable_result_t<Cpo, Target, ArgN...> {
+    return self((Target&&) target);
+  }
+
+  struct compose {
+    template (typename Target, typename Other, typename Self)
+    (requires (!std::is_base_of_v<instance_of_apply_target, remove_cvref_t<Target>>) AND
+        std::is_base_of_v<instance_of_apply_target, remove_cvref_t<Other>> AND
+        std::is_same_v<remove_cvref_t<Self>, type>)
+    auto operator()(Target&& target, Other&& other, Self&& self) const 
+      noexcept(
+        is_nothrow_callable_v<Other, Target> &&
+        is_nothrow_callable_v<Self, callable_result_t<Other, Target>>
+      ) 
+      -> callable_result_t<Self, callable_result_t<Other, Target>> {
+      return ((Self&&)self)(((Other&&)other)((Target&&)target));
+    }
+  };
+
+  template (typename Other, typename Self)
+    (requires std::is_base_of_v<instance_of_apply_target, remove_cvref_t<Other>> AND
+      std::is_same_v<remove_cvref_t<Self>, type>)
+  friend apply_target<compose, Other, Self> 
+  operator|(Other&& other, Self&& self) 
+    noexcept(
+      std::is_nothrow_constructible_v<
+        apply_target<compose, Other, Self>,
+        Other,
+        Self
+      >) {
+    return apply_target<compose, Other, Self>{
+      (Other&&)other, (Self&&)self};
   }
 };
 
