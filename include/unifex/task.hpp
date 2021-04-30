befeeaf8e9c44afebdc0441f596fbfef3bc77c09
Eric Niebler <eniebler@boost.org>
2021-04-29 17:06:35 -0700

    unsubscribe from task's inplace stop token adaptor on unhandled done (#272)

--- include/unifex/task.hpp
+++ include/unifex/task.hpp
@@ -221,7 +221,7 @@ struct _awaiter {
   private:
     coro::coroutine_handle<ThisPromise> coro_;
     UNIFEX_NO_UNIQUE_ADDRESS
-    inplace_stop_token_adapter<stop_token_type_t<OtherPromise>> stopTokenAdapter_;
+    detail::inplace_stop_token_adapter_subscription<stop_token_type_t<OtherPromise>> stopTokenAdapter_;
   };
 };
 
@@ -264,7 +264,7 @@ private:
       : coro_(h) {}
 
   template<typename Promise>
-  friend awaiter<Promise> tag_invoke(tag_t<unifex::await_transform>, Promise& promise, type&& t) noexcept {
+  friend awaiter<Promise> tag_invoke(tag_t<unifex::await_transform>, Promise&, type&& t) noexcept {
     return awaiter<Promise>{std::exchange(t.coro_, {})};
   }
 
