a948e2a9a0f1818d426110806a617f003ba3b727
Maikel Nadolski <maikel@nadolski.berlin>
2021-05-04 04:54:21 +0200

    Use gtest iff BUILD_TESTING=ON (#254)
    * Use gtest iff BUILD_TESTING=ON
    
    * Improve comment for using enable_testing()
    
    * Check if this CMakeLists is the top-level project.
    
    * Add a reference for project-is-top-level.cmake
    
    * Add a comment for the lirbary target and don't use an early return.
    
    * Fix typos in comment

---
+++ cmake/project-is-top-level.cmake
@@ -0,0 +1,7 @@
+# Taken from: https://github.com/friendlyanon/cmake-init-executable
+# This variable is set by project() in CMake 3.21+
+string(
+    COMPARE EQUAL
+    "${CMAKE_SOURCE_DIR}" "${PROJECT_SOURCE_DIR}"
+    PROJECT_IS_TOP_LEVEL
+)
