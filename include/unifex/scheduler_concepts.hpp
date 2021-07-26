1f3eacb483032bf36a122af36afaffbb7a57463d
Eric Niebler <eniebler@boost.org>
2021-07-26 12:45:32 -0500

    move deduction guide to namespace scope for gcc-10

--- include/unifex/scheduler_concepts.hpp
+++ include/unifex/scheduler_concepts.hpp
@@ -461,35 +461,37 @@ namespace _now {
 using _now::now;
 
 namespace _current {
+#if !UNIFEX_NO_COROUTINES
+  template <typename Scheduler>
+  struct _awaiter {
+    Scheduler sched_;
+
+    static constexpr bool await_ready() noexcept {
+      return true;
+    }
+    void await_suspend(coro::coroutine_handle<>) const noexcept {
+    }
+    Scheduler await_resume() {
+      return (Scheduler&&) sched_;
+    }
+    friend constexpr auto tag_invoke(tag_t<unifex::blocking>, const _awaiter&) noexcept {
+      return blocking_kind::always_inline;
+    }
+  };
+  template <typename Scheduler>
+  _awaiter(Scheduler) -> _awaiter<Scheduler>;
+#endif // !UNIFEX_NO_COROUTINES
+
   struct _scheduler {
 #if !UNIFEX_NO_COROUTINES
   private:
-    template <typename Scheduler>
-    struct _awaiter {
-      Scheduler sched_;
-
-      static constexpr bool await_ready() noexcept {
-        return true;
-      }
-      void await_suspend(coro::coroutine_handle<>) const noexcept {
-      }
-      Scheduler await_resume() {
-        return (Scheduler&&) sched_;
-      }
-      friend constexpr auto tag_invoke(tag_t<unifex::blocking>, const _awaiter&) noexcept {
-        return blocking_kind::always_inline;
-      }
-    };
-    template <typename Scheduler>
-    _awaiter(Scheduler) -> _awaiter<Scheduler>;
-
     // `co_await current_scheduler()` to fetch a coroutine's current scheduler.
     template (typename Tag, typename Promise)
       (requires same_as<Tag, tag_t<await_transform>> AND scheduler_provider<Promise&>)
     friend auto tag_invoke(Tag, Promise& promise, _scheduler) noexcept {
       return _awaiter{get_scheduler(promise)};
     }
-#endif
+#endif // !UNIFEX_NO_COROUTINES
 
   public:
     auto schedule() const noexcept {
