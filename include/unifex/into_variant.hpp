1d4199d3e1fbfb7f27d3a7350547d044e3cc18b6
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-09-18 00:18:55 +0530

    added into_variant implementation

---
+++ include/unifex/into_variant.hpp
@@ -0,0 +1,136 @@
+#pragma once
+
+#include <unifex/config.hpp>
+#include <unifex/async_trace.hpp>
+#include <unifex/bind_back.hpp>
+#include <unifex/receiver_concepts.hpp>
+#include <unifex/sender_concepts.hpp>
+#include <unifex/type_list.hpp>
+
+#include <tuple>
+#include <variant>
+
+#include <unifex/detail/prologue.hpp>
+
+namespace unifex {
+namespace _into_variant {
+template <typename Receiver>
+struct _receiver {
+  struct type;
+};
+template <typename Receiver>
+using receiver_t = typename _receiver<Receiver>::type;
+
+template <typename Receiver>
+struct _receiver<Receiver>::type {
+  UNIFEX_NO_UNIQUE_ADDRESS Receiver receiver_;
+
+  template(typename... Values)(
+  requires receiver_of< Receiver, Values...>)
+  void set_value(Values&&... values) && {
+    unifex::set_value((Receiver &&)(receiver_),
+        std::variant<std::tuple<remove_cvref_t<Values>...>>(
+        std::make_tuple((Values &&)(values)...)));
+  }
+
+  template(typename Error)
+  (requires receiver<Receiver, Error>)
+  void set_error(Error&& error) && noexcept {
+    unifex::set_error((Receiver &&)(receiver_), (Error &&)(error));
+  }
+
+  void set_done() && noexcept {
+    unifex::set_done((Receiver &&)(receiver_));
+  }
+
+  template(typename CPO)
+  (requires is_receiver_query_cpo_v<CPO> AND is_callable_v<CPO, const Receiver&>)
+  friend auto tag_invoke(CPO cpo, const type& r)
+  noexcept(is_nothrow_callable_v<CPO, const Receiver&>)
+  -> callable_result_t<CPO, const Receiver&> {
+    return std::move(cpo)(std::as_const(r.receiver_));
+  }
+
+  template <typename Visit>
+  friend void
+  tag_invoke(tag_t<visit_continuations>, const type& self, Visit&& visit) {
+    std::invoke(visit, self.receiver_);
+  }
+};
+
+template <typename Predecessor>
+struct _sender {
+  struct type;
+};
+template <typename Predecessor>
+using sender = typename _sender<remove_cvref_t<Predecessor>>::type;
+
+template <typename Predecessor>
+struct _sender<Predecessor>::type {
+  UNIFEX_NO_UNIQUE_ADDRESS Predecessor pred_;
+
+  template <
+      template <typename...>
+      class Variant,
+      template <typename...>
+      class Tuple>
+  using value_types = Variant<
+      Tuple<sender_value_types_t<Predecessor, std::variant, std::tuple>>>;
+
+  template <template <typename...> class Variant>
+  using error_types = sender_error_types_t<Predecessor, Variant>;
+
+  static constexpr bool sends_done = sender_traits<Predecessor>::sends_done;
+
+  friend constexpr auto tag_invoke(tag_t<blocking>, const type& sender) {
+    return blocking(sender.pred_);
+  }
+
+  template(typename Sender, typename Receiver)(
+      requires same_as<remove_cvref_t<Sender>, type> AND receiver<Receiver> AND
+      sender_to<member_t<Sender, Predecessor>, receiver_t<remove_cvref_t< Receiver>>>)
+  friend auto tag_invoke(tag_t<unifex::connect>, Sender&& s, Receiver&& r) 
+  noexcept(std::is_nothrow_constructible_v<remove_cvref_t<Receiver>, Receiver>&&
+  is_nothrow_connectable_v<member_t< Sender, Predecessor>,
+  receiver_t<remove_cvref_t<Receiver>>>)
+  -> connect_result_t<member_t<Sender, Predecessor>, receiver_t<remove_cvref_t<Receiver>>> {
+    return unifex::connect(static_cast<Sender&&>(s).pred_,
+        receiver_t<remove_cvref_t<Receiver>>{static_cast<Receiver&&>(r)});
+  }
+};
+
+namespace _cpo {
+struct _fn {
+private:
+  template <typename Sender>
+  using _result_t = typename conditional_t<
+      tag_invocable<_fn, Sender>,
+      meta_tag_invoke_result<_fn>,
+      meta_quote1<_into_variant::sender>>::template apply<Sender>;
+
+public:
+  template(typename Sender)(requires tag_invocable<_fn, Sender>) auto
+  operator()(Sender&& predecessor) const
+  noexcept(is_nothrow_tag_invocable_v<_fn, Sender>) -> _result_t<Sender> {
+    return unifex::tag_invoke(_fn{}, (Sender &&)(predecessor));
+  }
+
+  template(typename Sender)
+  (requires(!tag_invocable<_fn, Sender>))
+  auto operator()(Sender&& predecessor) const 
+  noexcept(std::is_nothrow_constructible_v<_into_variant::sender<Sender>, Sender>)
+  -> _result_t<Sender> {
+    return _into_variant::sender<Sender>{(Sender &&)(predecessor)};
+  }
+  constexpr auto operator()() const
+  noexcept(is_nothrow_callable_v<tag_t<bind_back>, _fn>) -> bind_back_result_t<_fn> {
+    return bind_back(*this);
+  }
+};
+}  // namespace _cpo
+
+}  // namespace _into_variant
+inline constexpr _into_variant::_cpo::_fn into_variant{};
+}  // namespace unifex
+
+#include <unifex/detail/epilogue.hpp>
---
+++ test/into_variant_test.cpp
@@ -0,0 +1,91 @@
+#include <unifex/then.hpp>
+#include <unifex/sync_wait.hpp>
+#include <unifex/when_all.hpp>
+#include <unifex/just_error.hpp>
+#include <unifex/just.hpp>
+#include <unifex/into_variant.hpp>
+#include <unifex/just_done.hpp>
+#include <exception>
+#include <variant>
+#include <gtest/gtest.h>
+
+using namespace unifex;
+
+template <typename... T>
+using vt_t = std::variant<std::tuple<T...>>;
+
+using std::variant;
+using std::tuple;
+using std::is_same_v;
+
+
+TEST(IntoVariant, StaticTypeCheck){
+  auto snd1 = just(42)
+            | into_variant();
+  static_assert(is_same_v<decltype(snd1)::value_types<variant, tuple>,
+      vt_t<vt_t<int>>>);
+  static_assert(is_same_v<decltype(snd1)::error_types<variant>,
+      variant<std::exception_ptr>>);
+  static_assert(decltype(snd1)::sends_done == false);
+
+  auto snd2 = just_error(42)
+            | into_variant();
+  static_assert(is_same_v<decltype(snd2)::value_types<variant, tuple>,
+      vt_t<variant<>>>);
+  static_assert(is_same_v<decltype(snd2)::error_types<variant>,
+      variant<int>>);
+  static_assert(decltype(snd2)::sends_done == false);
+
+  auto snd3 = just_done()
+            | into_variant();
+  static_assert(is_same_v<decltype(snd3)::value_types<variant, tuple>,
+      vt_t<variant<>>>);
+  static_assert(is_same_v<decltype(snd3)::error_types<variant>,
+      variant<>>);
+  static_assert(decltype(snd3)::sends_done == true);
+
+  auto snd4 = when_all( just(42), just(42.0), just_error(42) )
+            | into_variant();
+  static_assert(is_same_v<decltype(snd4)::value_types<variant, tuple>,
+      vt_t<vt_t<vt_t<int>, vt_t<double>, variant<>>>>);
+  static_assert(is_same_v<decltype(snd4)::error_types<variant>,
+      variant<std::exception_ptr, int>>);
+  static_assert(decltype(snd4)::sends_done == true);
+}
+
+TEST(IntoVariant, Working){
+  bool called = false;
+  auto x = sync_wait(into_variant(when_all(
+          just(42),
+          just(42.0) | then([&](double d){
+            called = true;
+            return d + 1;
+          }))));
+  EXPECT_EQ(called, true);
+  EXPECT_TRUE(x.has_value());
+  const auto& [vt_first_val, vt_second_val] = std::get<0>(x.value());
+  const auto& [first_val] = std::get<0>(vt_first_val);
+  const auto [second_val] = std::get<0>(vt_second_val);
+  EXPECT_EQ(first_val, 42);
+  EXPECT_EQ(second_val, 43.0);
+}
+
+TEST(IntoVariant, Pipeable){
+  bool called = false;
+  auto x = when_all(
+      just(42),
+      just(42.5)
+      | then([&](double d){
+          called = true;
+          return d + 1;
+        }))
+      | into_variant()
+      | sync_wait();
+  EXPECT_EQ(called, true);
+  EXPECT_TRUE(x.has_value());
+  const auto& [vt_first_val, vt_second_val] = std::get<0>(x.value());
+  const auto& [first_val] = std::get<0>(vt_first_val);
+  const auto [second_val] = std::get<0>(vt_second_val);
+  EXPECT_EQ(first_val, 42);
+  EXPECT_EQ(second_val, 43.5);
+}
