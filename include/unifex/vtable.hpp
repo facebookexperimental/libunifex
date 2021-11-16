7d33ba3e56a162ac2b21f61c91ee1fd29ae99ab6
Patrick Williams <patrick@stwcx.xyz>
2021-11-16 13:16:21 -0600

    vtable: non-virtual runtime polymorphism support
    Introduce a header to facilitate conversion from virtual base
    classes to a vtable for runtime polymorphic functionality.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

---
+++ include/unifex/vtable.hpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <functional>
+#include <type_traits>
+
+#include <unifex/detail/concept_macros.hpp>
+
+#define UNIFEX_VTABLE_DECLARE                 \
+  operator bool() { return ctx_ != nullptr; } \
+  void* ctx_ = nullptr
+
+#define UNIFEX_VTABLE_ENTRY(entry, ret, /*types*/...)   \
+  using entry##_t = ret (*)(void*, __VA_ARGS__);        \
+  template <typename... Args>                           \
+  ret entry(Args&&... args) {                           \
+    return entry##_(ctx_, std::forward<Args>(args)...); \
+  }                                                     \
+  entry##_t entry##_ = nullptr
+
+#define UNIFEX_VTABLE_ENTRY_VOID(entry, ret) \
+  using entry##_t = ret (*)(void*);          \
+  ret entry() { return entry##_(ctx_); };    \
+  entry##_t entry##_ = nullptr
+
+#define UNIFEX_VTABLE_CONSTRUCT_FN(fn) \
+  (unifex::_vtable::                   \
+       _construct_indirect<std::remove_reference_t<decltype(*this)>, fn>(fn)),
+
+#define UNIFEX_VTABLE_CONSTRUCT(/*functions*/...) \
+  { this, UNIFEX_PP_FOR_EACH(UNIFEX_VTABLE_CONSTRUCT_FN, __VA_ARGS__) }
+
+namespace unifex {
+namespace _vtable {
+
+template <typename T, auto Key, typename Ret, typename... Args>
+static constexpr auto _construct_indirect(Ret (T::*)(Args...)) noexcept {
+  constexpr auto f = [](void* ctx, Args... args) {
+    return std::invoke(Key, static_cast<T*>(ctx), std::forward<Args>(args)...);
+  };
+  return f;
+}
+
+template <typename T, auto Key, typename Ret>
+static constexpr auto _construct_indirect(Ret (T::*)()) noexcept {
+  constexpr auto f = [](void* ctx) {
+    return std::invoke(Key, static_cast<T*>(ctx));
+  };
+  return f;
+}
+
+}  // namespace _vtable
+}  // namespace unifex
---
+++ test/vtable_test.cpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <iostream>
+#include <memory>
+
+#include <unifex/vtable.hpp>
+
+#include <gtest/gtest.h>
+
+struct simple_vtable_t {
+  UNIFEX_VTABLE_DECLARE;
+
+  UNIFEX_VTABLE_ENTRY_VOID(foo, int);
+  UNIFEX_VTABLE_ENTRY(bar, int, double, double);
+};
+
+struct class_one {
+  int do_foo() noexcept { return 1; }
+  int do_bar(double, double) { return 0; }
+
+  simple_vtable_t table_ =
+      UNIFEX_VTABLE_CONSTRUCT(&class_one::do_foo, &class_one::do_bar);
+};
+
+struct class_two {
+  explicit class_two(int v) : value_(v){};
+  class_two() = default;
+
+  int value_ = 2;
+
+  int foo_fn() noexcept { return value_; }
+  int bar_fn(double, double) { return 1; }
+
+  simple_vtable_t table_ =
+      UNIFEX_VTABLE_CONSTRUCT(&class_two::foo_fn, &class_two::bar_fn);
+};
+
+TEST(vtable, bar_returns_expected) {
+  auto instance_0 = std::make_unique<class_one>();
+  ASSERT_TRUE(!!instance_0->table_);
+  EXPECT_EQ(0, instance_0->table_.bar(0, 0));
+
+  auto instance_1 = std::make_unique<class_two>();
+  ASSERT_TRUE(!!instance_1->table_);
+  EXPECT_EQ(1, instance_1->table_.bar(0, 0));
+}
+
+TEST(vtable, foo_indirect_goes_to_instance) {
+  auto instance_0 = std::make_unique<class_one>();
+  auto instance_1 = std::make_unique<class_two>();
+  auto instance_2 = std::make_unique<class_two>(3);
+
+  simple_vtable_t* tables[] = {
+      &instance_0->table_, &instance_1->table_, &instance_2->table_};
+
+  for (auto& i : tables) {
+    ASSERT_TRUE(!!i);
+  }
+
+  EXPECT_EQ(tables[0]->foo(), 1);
+  EXPECT_EQ(tables[1]->foo(), 2);
+  EXPECT_EQ(tables[2]->foo(), 3);
+}
+
+struct reference_vtable_t {
+  UNIFEX_VTABLE_DECLARE;
+  UNIFEX_VTABLE_ENTRY(foo, std::string, std::string&&);
+  UNIFEX_VTABLE_ENTRY(bar, std::string, std::string&);
+};
+
+struct class_three {
+  std::string foo(std::string&& s) { return std::move(s); };
+  std::string bar(std::string& s) {
+    auto r = s;
+    s = "";
+    return r;
+  };
+
+  reference_vtable_t table_ =
+      UNIFEX_VTABLE_CONSTRUCT(&class_three::foo, &class_three::bar);
+};
+
+TEST(vtable, perfect_forwarding) {
+  auto instance = std::make_unique<class_three>();
+  ASSERT_TRUE(!!instance->table_);
+
+  static constexpr auto a = "string a";
+  std::string ao = a;
+  auto ar = instance->table_.foo(std::move(ao));
+  EXPECT_EQ(a, ar);
+
+  static constexpr auto b = "string b";
+  std::string bo = b;
+  auto br = instance->table_.bar(bo);
+  EXPECT_EQ(b, br);
+  EXPECT_EQ("", bo);
+}
