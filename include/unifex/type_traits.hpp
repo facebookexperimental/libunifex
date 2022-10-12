7ad8ba5708d9a1fb02197ef9e8fff63b8fd2049e
Ian Petersen <ispeters@gmail.com>
2022-10-12 13:48:32 -0700

    Introduce spawn_detached(sender, scope, allocator)
    This diff introduces a new algorithm, `unifex::spawn_detached()`.
    `spawn_detached` takes a sender, an "async scope", and an optional
    allocator.  It nests the sender in the scope with `unifex::nest`,
    allocates and operation state using the allocator, and starts that
    operation.  The given async scope may be anything that `nest()`
    supports, which currently includes both `v1::async_scope` and
    `v2::async_scope`.

--- include/unifex/type_traits.hpp
+++ include/unifex/type_traits.hpp
@@ -161,6 +161,23 @@ inline constexpr bool is_nothrow_callable_v =
 template <typename Fn, typename... As>
 struct is_nothrow_callable : std::bool_constant<is_nothrow_callable_v<Fn, As...>> {};
 
+template <typename T, typename = void>
+struct is_allocator : std::false_type {};
+
+template <typename T>
+constexpr bool is_allocator_v = is_allocator<T>::value;
+
+template <typename T>
+struct is_allocator<
+    T,
+    std::void_t<typename T::value_type,
+                decltype(std::declval<T>().allocate(std::size_t{})),
+                decltype(std::declval<T>().deallocate(nullptr, std::size_t{})),
+                std::enable_if_t<std::is_copy_constructible_v<T>>,
+                decltype(std::declval<const T&>() == std::declval<const T&>()),
+                decltype(std::declval<const T&>() != std::declval<const T&>())>>
+    : std::true_type {};
+
 template <typename T>
 struct type_always {
   template <typename...>
