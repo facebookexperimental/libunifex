3dfeac327fc10837c252d386a307dc2d68d6e55a
Ian Petersen <ispeters@gmail.com>
2023-05-16 16:09:01 -0700

    Restrict previous warning suppression to GCC 9

--- include/unifex/v1/async_scope.hpp
+++ include/unifex/v1/async_scope.hpp
@@ -55,14 +55,16 @@ struct _attach_op_base final {
 
 template <typename Receiver>
 struct _attach_op_base<Receiver>::type {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+UNIFEX_DIAGNOSTIC_PUSH
+UNIFEX_INGORE_MAYBE_UNINITIALIZED_IN_GCC_9
+
   template <typename Receiver2>
   explicit type(inplace_stop_token stoken, Receiver2&& receiver) noexcept(
       std::is_nothrow_constructible_v<Receiver, Receiver2>)
     : stoken_(stoken)
     , receiver_(static_cast<Receiver2&&>(receiver)) {}
-#pragma GCC diagnostic pop
+
+UNIFEX_DIAGNOSTIC_POP
 
   type(type&&) = delete;
 
