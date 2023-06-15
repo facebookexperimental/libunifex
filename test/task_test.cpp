facebookexperimental/libunifex

commit 351a97c1f583f62956a93867542b785d674d7eeb

task<> scheduler affinity (#290)
This commit implements scheduler affinity -- aka "sticky" scheduling -- in `unifex::task<>`. The idea is that it is impossible for a child operation to cause the current coroutine to resume on the wrong execution context.

* `task<>`-based coroutines track and propagate the current scheduler
* `at_coroutine_exit` remembers current scheduler from when the cleanup action is scheduled
* `schedule` always returns an instance of `sender_for<schedule, the_sender>`,
  which is also a `scheduler_provider`
* scheduler affinity when co_await-ing senders in a `task<>`-returning coroutine
* scheduler affinity when co_await-ing awaitables in a `task<>`-returning coroutine
* awaitables and senders that are `blocking_kind::always_inline` don't get a thunk
* More senders and awaitables support compile-time blocking queries
* `co_await schedule(sched)` is magic in a `task<>`-returning coroutine: it changes execution
  context and schedules a cleanup action to transition back to the original scheduler

include/unifex/connect_awaitable.hpp
@@ -93,17 +93,13 @@ public:
 
     template <typename Value>
     auto await_transform(Value&& value) -> decltype(auto) {
-      if constexpr (callable<decltype(unifex::await_transform), promise_type&, Value>) {
-        return unifex::await_transform(*this, (Value&&)value);
-      } else {
-        return Value((Value &&) value);
-      }
+      return unifex::await_transform(*this, (Value&&) value);
     }
 
     template <typename Func>
     friend void
     tag_invoke(tag_t<visit_continuations>, const promise_type& p, Func&& func) {
-      visit_continuations(p.receiver_, (Func&&)func);
+      visit_continuations(p.receiver_, (Func&&) func);
     }
 
     template(typename CPO)
@@ -140,21 +136,12 @@ public:
 } // namespace _await
 
 namespace _await_cpo {
-  template<typename... Ts>
-  using count_types = std::integral_constant<std::size_t, sizeof...(Ts)>;
-
-  template<typename Receiver>
-  struct set_value_applicator {
-    Receiver& receiver_;
-
-    template<typename... Values>
-    void operator()(Values&&... values) {
-      unifex::set_value(std::move(receiver_), (Values&&)values...);
-    }
-  };
-
   inline const struct _fn {
   private:
+    template <typename Awaitable>
+    using awaitable_single_value_result_t =
+        non_void_t<wrap_reference_t<decay_rvalue_t<await_result_t<Awaitable>>>>;
+
     struct _comma_hack {
       template <typename T>
       friend T&& operator,(T&& t, _comma_hack) noexcept {
@@ -162,6 +149,7 @@ namespace _await_cpo {
       }
       operator unit() const noexcept { return {}; }
     };
+
     template <typename Awaitable, typename Receiver>
     static auto connect_impl(Awaitable awaitable, Receiver receiver)
         -> _await::sender_task<Receiver> {
@@ -170,7 +158,13 @@ namespace _await_cpo {
       try {
 #endif // !UNIFEX_NO_EXCEPTIONS
 
-        using result_type = sender_single_value_result_t<Awaitable>;
+        // The _sender_task's promise type has an await_transform that passes the
+        // awaitable through unifex::await_transform. So take that into consideration
+        // when computing the result type:
+        using promise_type = typename _await::sender_task<Receiver>::promise_type;
+        using awaitable_type =
+            callable_result_t<tag_t<unifex::await_transform>, promise_type&, Awaitable>;
+        using result_type = awaitable_single_value_result_t<awaitable_type>;
 
         // This is a bit mind bending control-flow wise.
         // We are first evaluating the co_await expression.
@@ -182,31 +176,17 @@ namespace _await_cpo {
         // for the receiver to destroy the coroutine.
         co_yield [&](result_type&& result) {
               return [&] {
-                constexpr size_t valueOverloadCount =
-                    sender_value_types_t<Awaitable, count_types, single_value_type>::value;
-                static_assert(valueOverloadCount <= 1);
-
-                if constexpr (valueOverloadCount == 1) {
-                  constexpr size_t valueCount =
-                      sender_value_types_t<Awaitable, type_identity_t, count_types>::value;
-                  if constexpr (valueCount == 0) {
-                    unifex::set_value(std::move(receiver));
-                  } else if constexpr (valueCount == 1) {
-                    unifex::set_value(std::move(receiver), static_cast<result_type&&>(result));
-                  } else {
-                    std::apply(set_value_applicator<Receiver>{receiver}, (result_type&&)result);
-                  }
+                if constexpr (std::is_void_v<await_result_t<awaitable_type>>) {
+                  unifex::set_value(std::move(receiver));
                 } else {
-                  // Shouldn't complete with a value if there are no value_types
-                  // specified.
-                  std::terminate();
+                  unifex::set_value(std::move(receiver), static_cast<result_type&&>(result));
                 }
               };
             // The _comma_hack here makes this well-formed when the co_await
             // expression has type void. This could potentially run into trouble
             // if the type of the co_await expression itself overloads operator
             // comma, but that's pretty unlikely.
-            }((co_await (Awaitable &&)awaitable, _comma_hack{}));
+            }((co_await (Awaitable &&) awaitable, _comma_hack{}));
 #if !UNIFEX_NO_EXCEPTIONS
       } catch (...) {
         ex = std::current_exception();
@@ -221,12 +201,92 @@ namespace _await_cpo {
     template <typename Awaitable, typename Receiver>
     auto operator()(Awaitable&& awaitable, Receiver&& receiver) const
       -> _await::sender_task<remove_cvref_t<Receiver>> {
-      return connect_impl((Awaitable&&)awaitable, (Receiver&&)receiver);
+      return connect_impl((Awaitable&&) awaitable, (Receiver&&) receiver);
     }
   } connect_awaitable{};
 } // namespace _await_cpo
 
 using _await_cpo::connect_awaitable;
+
+// as_sender, for adapting an awaitable to be a typed sender
+namespace _as_sender {
+  template <typename Awaitable, typename Result = await_result_t<Awaitable>>
+  struct _sndr {
+    struct type {
+      template <
+          template <typename...> class Variant,
+          template <typename...> class Tuple>
+      using value_types = Variant<Tuple<Result>>;
+
+      template <template <typename...> class Variant>
+      using error_types = Variant<std::exception_ptr>;
+
+      static constexpr bool sends_done = true;
+
+      type(Awaitable awaitable)
+        : awaitable_((Awaitable&&) awaitable)
+      {}
+
+      template (typename Receiver)
+        (requires receiver_of<Receiver, Result>)
+      friend auto tag_invoke(tag_t<unifex::connect>, type&& t, Receiver&& r) {
+        return unifex::connect_awaitable(((type&&) t).awaitable_, (Receiver&&) r);
+      }
+
+      friend constexpr auto tag_invoke(tag_t<unifex::blocking>, const type& t) noexcept {
+        return unifex::blocking(t.awaitable_);
+      }
+    private:
+      Awaitable awaitable_;
+    };
+  };
+
+  template <typename Awaitable>
+  struct _sndr<Awaitable, void> {
+    struct type {
+      template <
+          template <typename...> class Variant,
+          template <typename...> class Tuple>
+      using value_types = Variant<Tuple<>>;
+
+      template <template <typename...> class Variant>
+      using error_types = Variant<std::exception_ptr>;
+
+      static constexpr bool sends_done = true;
+
+      explicit type(Awaitable awaitable)
+          noexcept(std::is_nothrow_move_constructible_v<Awaitable>)
+        : awaitable_((Awaitable&&) awaitable)
+      {}
+
+      template (typename Receiver)
+        (requires receiver_of<Receiver>)
+      friend auto tag_invoke(tag_t<unifex::connect>, type&& t, Receiver&& r) {
+        return unifex::connect_awaitable(((type&&) t).awaitable_, (Receiver&&) r);
+      }
+
+      friend constexpr auto tag_invoke(tag_t<unifex::blocking>, const type& t) noexcept {
+        return unifex::blocking(t.awaitable_);
+      }
+    private:
+      Awaitable awaitable_;
+    };
+  };
+
+  template <typename Awaitable>
+  using _sender = typename _sndr<Awaitable>::type;
+
+  struct _fn {
+    template (typename Awaitable)
+      (requires detail::_awaitable<Awaitable>)
+    _sender<remove_cvref_t<Awaitable>> operator()(Awaitable&& awaitable) const {
+      return _sender<remove_cvref_t<Awaitable>>{(Awaitable&&) awaitable};
+    }
+  };
+} // namespace _as_sender
+
+// Transforms an awaitable into a typed sender:
+inline constexpr _as_sender::_fn as_sender {};
 
 } // namespace unifex
 

include/unifex/allocate.hpp
@@ -22,6 +22,7 @@
 #include <unifex/sender_concepts.hpp>
 #include <unifex/tag_invoke.hpp>
 #include <unifex/bind_back.hpp>
+#include <unifex/blocking.hpp>
 
 #include <memory>
 #include <type_traits>
@@ -103,6 +104,10 @@ namespace _alloc {
           connect_result_t<member_t<Self, Sender>, Receiver>,
           remove_cvref_t<get_allocator_t<Receiver>>>{
           static_cast<Self&&>(s).sender_, (Receiver &&) r};
+    }
+
+    friend constexpr auto tag_invoke(tag_t<unifex::blocking>, const type& self) noexcept {
+      return blocking(self.sender_);
     }
 
     Sender sender_;

include/unifex/any_scheduler.hpp
@@ -198,6 +198,10 @@ struct _with<CPOs...>::any_scheduler {
     return _sender{this};
   }
 
+  type_index type() const noexcept {
+    return _get_type_index(impl_);
+  }
+
   friend _equal_to_fn;
   friend bool operator==(const any_scheduler& left, const any_scheduler& right) noexcept {
     return _equal_to(left.impl_, right);
@@ -211,7 +215,11 @@ private:
 };
 
 template <typename... CPOs>
-using any_scheduler_ref_impl = any_ref_t<_schedule_and_connect<CPOs...>>;
+using any_scheduler_ref_impl =
+    any_ref_t<
+        _schedule_and_connect<CPOs...>,
+        _get_type_index,
+        overload<bool(const this_&, const any_scheduler_ref<CPOs...>&) noexcept>(_equal_to)>;
 
 template <typename... CPOs>
 struct _with<CPOs...>::any_scheduler_ref {
@@ -257,6 +265,11 @@ struct _with<CPOs...>::any_scheduler_ref {
     return _sender{this};
   }
 
+  type_index type() const noexcept {
+    return _get_type_index(impl_);
+  }
+
+  // Shallow equality comparison by default, for regularity:
   friend bool operator==(const any_scheduler_ref& left, const any_scheduler_ref& right) noexcept {
     return left.impl_ == right.impl_;
   }
@@ -264,7 +277,14 @@ struct _with<CPOs...>::any_scheduler_ref {
     return !(left == right);
   }
 
+  // Deep equality comparison:
+  friend _equal_to_fn;
+  bool equal_to(const any_scheduler_ref& that) const noexcept {
+    return _equal_to(impl_, that);
+  }
+
 private:
+
   any_scheduler_ref_impl<CPOs...> impl_;
 };
 

include/unifex/async_trace.hpp
@@ -103,7 +103,7 @@ namespace _async_trace {
       return operation<Receiver>{(Receiver &&) r};
     }
 
-    friend blocking_kind tag_invoke(tag_t<blocking>, const sender&) noexcept {
+    friend auto tag_invoke(tag_t<blocking>, const sender&) noexcept {
       return blocking_kind::always_inline;
     }
   };

include/unifex/at_coroutine_exit.hpp
@@ -19,7 +19,11 @@
 #include <unifex/tag_invoke.hpp>
 #include <unifex/await_transform.hpp>
 #include <unifex/continuations.hpp>
+#include <unifex/scheduler_concepts.hpp>
 #include <unifex/stop_token_concepts.hpp>
+#include <unifex/inline_scheduler.hpp>
+#include <unifex/any_scheduler.hpp>
+#include <unifex/blocking.hpp>
 
 #if UNIFEX_NO_COROUTINES
 # error "Coroutine support is required to use this header"
@@ -110,11 +114,19 @@ struct _cleanup_promise_base {
     visit_continuations(p.continuation_, (Func &&) func);
   }
 
-  friend unstoppable_token tag_invoke(tag_t<get_stop_token>, const _cleanup_promise_base&) noexcept {
+  friend unstoppable_token
+  tag_invoke(tag_t<get_stop_token>, const _cleanup_promise_base&) noexcept {
     return unstoppable_token{};
   }
 
+  friend any_scheduler_ref
+  tag_invoke(tag_t<get_scheduler>, const _cleanup_promise_base& p) noexcept {
+    return p.sched_;
+  }
+
+  inline static constexpr inline_scheduler _default_scheduler{};
   continuation_handle<> continuation_{};
+  any_scheduler_ref sched_{_default_scheduler};
   bool isUnhandledDone_{false};
 };
 
@@ -139,6 +151,15 @@ struct _die_on_done_rec {
     [[noreturn]] void set_done() && noexcept {
       UNIFEX_ASSERT(!"A cleanup action tried to cancel. Calling terminate...");
       std::terminate();
+    }
+
+    template(typename CPO)
+      (requires is_receiver_query_cpo_v<CPO> AND
+                is_callable_v<CPO, const Receiver&>)
+    friend auto tag_invoke(CPO cpo, const type& p)
+        noexcept(is_nothrow_callable_v<CPO, const Receiver&>)
+        -> callable_result_t<CPO, const Receiver&> {
+      return cpo(p.rec_);
     }
   };
 };
@@ -172,7 +193,7 @@ struct _die_on_done {
           _die_on_done_rec_t<Receiver>{(Receiver&&) rec});
     }
 
-    Sender sender_;
+    UNIFEX_NO_UNIQUE_ADDRESS Sender sender_;
   };
 };
 
@@ -224,7 +245,7 @@ struct _cleanup_promise : _cleanup_promise_base {
     return unifex::await_transform(*this, _die_on_done_fn{}((Value&&) value));
   }
 
-  std::tuple<Ts&...> args_;
+  UNIFEX_NO_UNIQUE_ADDRESS std::tuple<Ts&...> args_;
 };
 
 template <typename... Ts>
@@ -249,11 +270,16 @@ struct [[nodiscard]] _cleanup_task {
   bool await_suspend(coro::coroutine_handle<Promise> parent) noexcept {
     continuation_.promise().continuation_ =
         exchange_continuation(parent.promise(), continuation_);
+    continuation_.promise().sched_ = get_scheduler(parent.promise());
     return false;
   }
 
   std::tuple<Ts&...> await_resume() noexcept {
     return std::exchange(continuation_, {}).promise().args_;
+  }
+
+  friend constexpr auto tag_invoke(tag_t<blocking>, const _cleanup_task&) noexcept {
+    return blocking_kind::always_inline;
   }
 
 private:
@@ -270,7 +296,7 @@ namespace _at_coroutine_exit {
   public:
     template (typename Action, typename... Ts)
       (requires callable<std::decay_t<Action>, std::decay_t<Ts>...>)
-    _cleanup_task<Ts...> operator()(Action&& action, Ts&&... ts) const {
+    _cleanup_task<std::decay_t<Ts>...> operator()(Action&& action, Ts&&... ts) const {
       return _fn::at_coroutine_exit((Action&&) action, (Ts&&) ts...);
     }
   } at_coroutine_exit{};

include/unifex/await_transform.hpp
@@ -85,13 +85,13 @@ struct _awaitable_base<Promise, Value>::type {
   struct _rec {
   public:
     explicit _rec(_expected<Value>* result, coro::coroutine_handle<Promise> continuation) noexcept
-    : result_(result)
-    , continuation_(continuation)
+      : result_(result)
+      , continuation_(continuation)
     {}
 
     _rec(_rec&& r) noexcept
-    : result_(std::exchange(r.result_, nullptr))
-    , continuation_(std::exchange(r.continuation_, nullptr))
+      : result_(std::exchange(r.result_, nullptr))
+      , continuation_(std::exchange(r.continuation_, nullptr))
     {}
 
     template(class... Us)
@@ -183,13 +183,16 @@ public:
 template <typename Promise, typename Sender>
 using _as_awaitable = typename _awaitable<Promise, Sender>::type;
 
-inline constexpr struct _fn {
+struct _fn {
   // Call custom implementation if present.
   template(typename Promise, typename Value)
     (requires tag_invocable<_fn, Promise&, Value>)
   auto operator()(Promise& promise, Value&& value) const
     noexcept(is_nothrow_tag_invocable_v<_fn, Promise&, Value>)
     -> tag_invoke_result_t<_fn, Promise&, Value> {
+    static_assert(detail::_awaitable<tag_invoke_result_t<_fn, Promise&, Value>>,
+        "The return type of a customization of unifex::await_transform() "
+        "must satisfy the awaitable concept.");
     return unifex::tag_invoke(_fn{}, promise, (Value&&)value);
   }
 
@@ -216,7 +219,7 @@ inline constexpr struct _fn {
       return (Value&&) value;
     }
   }
-} await_transform {};
+};
 
 } // namespace _await_tfx
 
@@ -229,7 +232,7 @@ inline constexpr struct _fn {
 //
 // Coroutine promise_types can implement their .await_transform() methods to
 // forward to this customisation point to enable use of type customisations.
-using _await_tfx::await_transform;
+inline constexpr _await_tfx::_fn await_transform {};
 
 } // namespace unifex
 

include/unifex/blocking.hpp
@@ -22,10 +22,11 @@
 
 namespace unifex {
 
-enum class blocking_kind {
+namespace _block {
+enum class _enum {
   // No guarantees about the timing and context on which the receiver will
   // be called.
-  maybe,
+  maybe = 0,
 
   // Always completes asynchronously.
   // Guarantees that the receiver will not be called on the current thread
@@ -44,8 +45,46 @@ enum class blocking_kind {
   always_inline
 };
 
-namespace _blocking {
-inline const struct _fn {
+struct blocking_kind {
+  template <_enum Kind>
+  using constant = std::integral_constant<_enum, Kind>;
+
+  blocking_kind() = default;
+
+  constexpr blocking_kind(_enum kind) noexcept
+    : value(kind)
+  {}
+
+  template <_enum Kind>
+  constexpr blocking_kind(constant<Kind>) noexcept
+    : value(Kind)
+  {}
+
+  constexpr operator _enum() const noexcept {
+    return value;
+  }
+
+  constexpr _enum operator()() const noexcept {
+    return value;
+  }
+
+  friend constexpr bool operator==(blocking_kind a, blocking_kind b) noexcept {
+    return a.value == b.value;
+  }
+
+  friend constexpr bool operator!=(blocking_kind a, blocking_kind b) noexcept {
+    return a.value != b.value;
+  }
+
+  static constexpr constant<_enum::maybe> maybe {};
+  static constexpr constant<_enum::never> never {};
+  static constexpr constant<_enum::always> always {};
+  static constexpr constant<_enum::always_inline> always_inline {};
+
+  _enum value{};
+};
+
+struct _fn {
   template(typename Sender)
     (requires tag_invocable<_fn, const Sender&>)
   constexpr auto operator()(const Sender& s) const
@@ -55,12 +94,32 @@ inline const struct _fn {
   }
   template(typename Sender)
     (requires (!tag_invocable<_fn, const Sender&>))
-  constexpr blocking_kind operator()(const Sender&) const noexcept {
+  constexpr auto operator()(const Sender&) const noexcept {
     return blocking_kind::maybe;
   }
-} blocking{};
-} // namespace _blocking
-using _blocking::blocking;
+};
+
+namespace _cfn {
+  template <_enum Kind>
+  static constexpr auto _kind(blocking_kind::constant<Kind> kind) noexcept {
+    return kind;
+  }
+  static constexpr auto _kind(blocking_kind) noexcept {
+    return blocking_kind::maybe;
+  }
+
+  template <typename T>
+  constexpr auto cblocking() noexcept {
+    using blocking_t = remove_cvref_t<decltype(_fn{}(UNIFEX_DECLVAL(T&)))>;
+    return _cfn::_kind(blocking_t{});
+  }
+}
+
+} // namespace _block
+
+inline constexpr _block::_fn blocking {};
+using _block::_cfn::cblocking;
+using _block::blocking_kind;
 
 } // namespace unifex
 

include/unifex/coroutine_concepts.hpp
@@ -59,7 +59,8 @@ struct await_result_impl<
 
 } // namespace detail
 
-inline const struct _get_awaiter_fn {
+namespace _get_awaiter {
+struct _fn {
   template <typename Awaitable>
   constexpr decltype(auto) operator()(Awaitable&& awaitable) const noexcept {
     if constexpr (detail::has_member_operator_co_await_v<Awaitable>) {
@@ -70,10 +71,13 @@ inline const struct _get_awaiter_fn {
       return static_cast<Awaitable&&>(awaitable);
     }
   }
-} get_awaiter {};
+};
+} // namespace _get_awaiter
+
+inline constexpr _get_awaiter::_fn get_awaiter {};
 
 template <typename Awaitable>
-using awaiter_type_t = decltype(get_awaiter(std::declval<Awaitable>()));
+using awaiter_type_t = remove_cvref_t<decltype(get_awaiter(std::declval<Awaitable>()))>;
 
 template <typename Awaitable>
 using await_result_t =

commit cd2696670196e4466939268ec4f0ed4dd9f94541

Fix regression in task<>
Prior to #290, task<> could co_await awaitables that returned references
in await_resume.

Test plan: added test case that passes at the commit before #290, then
fails after #290, and passes again with the changes in this commit.
I also added a test for task<> awaiting a sender that sends a
reference, but this was not actually broken.

include/unifex/connect_awaitable.hpp
@@ -212,7 +212,12 @@ using _await_cpo::connect_awaitable;
 
 // as_sender, for adapting an awaitable to be a typed sender
 namespace _as_sender {
-  template <typename Awaitable, typename Result = await_result_t<Awaitable>>
+
+  template <typename Awaitable>
+  using _awaitable_single_value_result_t =
+    wrap_reference_t<decay_rvalue_t<await_result_t<Awaitable>>>;
+
+  template <typename Awaitable, typename Result = _awaitable_single_value_result_t<Awaitable>>
   struct _sndr {
     struct type {
       template <

test/task_test.cpp
@@ -1,0 +1,65 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <unifex/coroutine.hpp>
+
+#if !UNIFEX_NO_COROUTINES
+
+#include <atomic>
+
+#include <unifex/just.hpp>
+#include <unifex/sync_wait.hpp>
+#include <unifex/task.hpp>
+#include <unifex/then.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace unifex;
+
+int global;
+
+task<int&> await_reference_awaitable() {
+  struct AwaitableGlobalRef {
+    bool await_ready() noexcept { return true; }
+    void await_suspend(coro::coroutine_handle<>) noexcept {};
+    int& await_resume() noexcept { return global; }
+  };
+  int& x = co_await AwaitableGlobalRef{};
+  co_return x;
+}
+
+task<int&> await_reference_sender() {
+  int& x = co_await then(just(), []() -> int& { return global; });
+  co_return x;
+}
+
+TEST(Task, AwaitAwaitableReturningReference) {
+  global = 0;
+  int& ref = sync_wait(await_reference_awaitable()).value();
+  EXPECT_EQ(ref, 0);
+  global = 10;
+  EXPECT_EQ(ref, 10);
+}
+
+TEST(Task, AwaitSenderReturningReference) {
+  global = 0;
+  int& ref = sync_wait(await_reference_sender()).value();
+  EXPECT_EQ(ref, 0);
+  global = 10;
+  EXPECT_EQ(ref, 10);
+}
+
+#endif // !UNIFEX_NO_COROUTINES