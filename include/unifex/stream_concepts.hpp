c9fe85da9dea2c60ab3aa397afd293cbd3835b63
Ian Petersen <ispeters@gmail.com>
2023-10-30 14:16:03 -0700

    Format stream_concepts.hpp

--- include/unifex/stream_concepts.hpp
+++ include/unifex/stream_concepts.hpp
@@ -15,70 +15,72 @@
  */
 #pragma once
 
-#include <unifex/tag_invoke.hpp>
 #include <unifex/sender_concepts.hpp>
+#include <unifex/tag_invoke.hpp>
 
 #include <unifex/detail/prologue.hpp>
 
 namespace unifex {
 namespace _streams {
-  inline const struct _next_fn {
-  private:
-    template <typename Stream>
-    using _member_next_result_t = decltype(UNIFEX_DECLVAL(Stream).next());
-    template <typename Stream>
-    using _result_t =
-      typename conditional_t<
-        tag_invocable<_next_fn, Stream>,
-        meta_tag_invoke_result<_next_fn>,
-        meta_quote1<_member_next_result_t>>::template apply<Stream>;
-  public:
-    template(typename Stream)
-      (requires tag_invocable<_next_fn, Stream&>)
-    auto operator()(Stream& stream) const
-        noexcept(is_nothrow_tag_invocable_v<_next_fn, Stream&>)
-        -> _result_t<Stream&> {
-      return unifex::tag_invoke(_next_fn{}, stream);
-    }
-    template(typename Stream)
-      (requires (!tag_invocable<_next_fn, Stream&>))
-    auto operator()(Stream& stream) const
-        noexcept(noexcept(stream.next()))
-        -> _result_t<Stream&> {
-      return stream.next();
-    }
-  } next{};
+inline const struct _next_fn {
+private:
+  template <typename Stream>
+  using _member_next_result_t = decltype(UNIFEX_DECLVAL(Stream).next());
+  template <typename Stream>
+  using _result_t = typename conditional_t<
+      tag_invocable<_next_fn, Stream>,
+      meta_tag_invoke_result<_next_fn>,
+      meta_quote1<_member_next_result_t>>::template apply<Stream>;
 
-  inline const struct _cleanup_fn {
-  private:
-    template <typename Stream>
-    using _member_cleanup_result_t = decltype(UNIFEX_DECLVAL(Stream).cleanup());
-    template <typename Stream>
-    using _result_t =
-      typename conditional_t<
-        tag_invocable<_cleanup_fn, Stream>,
-        meta_tag_invoke_result<_cleanup_fn>,
-        meta_quote1<_member_cleanup_result_t>>::template apply<Stream>;
-  public:
-    template(typename Stream)
-      (requires tag_invocable<_cleanup_fn, Stream&>)
-    auto operator()(Stream& stream) const
-        noexcept(is_nothrow_tag_invocable_v<_cleanup_fn, Stream&>)
-        -> _result_t<Stream&> {
-      return unifex::tag_invoke(_cleanup_fn{}, stream);
-    }
-    template(typename Stream)
-      (requires (!tag_invocable<_cleanup_fn, Stream&>))
-    auto operator()(Stream& stream) const
-        noexcept(noexcept(stream.cleanup()))
-        -> _result_t<Stream&> {
-      return stream.cleanup();
-    }
-  } cleanup{};
-} // namespace _streams
+public:
+  template(typename Stream)                        //
+      (requires tag_invocable<_next_fn, Stream&>)  //
+      auto
+      operator()(Stream& stream) const
+      noexcept(is_nothrow_tag_invocable_v<_next_fn, Stream&>)
+          -> _result_t<Stream&> {
+    return unifex::tag_invoke(_next_fn{}, stream);
+  }
+  template(typename Stream)                          //
+      (requires(!tag_invocable<_next_fn, Stream&>))  //
+      auto
+      operator()(Stream& stream) const noexcept(noexcept(stream.next()))
+          -> _result_t<Stream&> {
+    return stream.next();
+  }
+} next{};
 
-using _streams::next;
+inline const struct _cleanup_fn {
+private:
+  template <typename Stream>
+  using _member_cleanup_result_t = decltype(UNIFEX_DECLVAL(Stream).cleanup());
+  template <typename Stream>
+  using _result_t = typename conditional_t<
+      tag_invocable<_cleanup_fn, Stream>,
+      meta_tag_invoke_result<_cleanup_fn>,
+      meta_quote1<_member_cleanup_result_t>>::template apply<Stream>;
+
+public:
+  template(typename Stream)                           //
+      (requires tag_invocable<_cleanup_fn, Stream&>)  //
+      auto
+      operator()(Stream& stream) const
+      noexcept(is_nothrow_tag_invocable_v<_cleanup_fn, Stream&>)
+          -> _result_t<Stream&> {
+    return unifex::tag_invoke(_cleanup_fn{}, stream);
+  }
+  template(typename Stream)                             //
+      (requires(!tag_invocable<_cleanup_fn, Stream&>))  //
+      auto
+      operator()(Stream& stream) const noexcept(noexcept(stream.cleanup()))
+          -> _result_t<Stream&> {
+    return stream.cleanup();
+  }
+} cleanup{};
+}  // namespace _streams
+
 using _streams::cleanup;
+using _streams::next;
 
 template <typename Stream>
 using next_sender_t = decltype(next(std::declval<Stream&>()));
@@ -90,8 +92,9 @@ template <typename Stream, typename Receiver>
 using next_operation_t = connect_result_t<next_sender_t<Stream>, Receiver>;
 
 template <typename Stream, typename Receiver>
-using cleanup_operation_t = connect_result_t<cleanup_sender_t<Stream>, Receiver>;
+using cleanup_operation_t =
+    connect_result_t<cleanup_sender_t<Stream>, Receiver>;
 
-} // namespace unifex
+}  // namespace unifex
 
 #include <unifex/detail/epilogue.hpp>
