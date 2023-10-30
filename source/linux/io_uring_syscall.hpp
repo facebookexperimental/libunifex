16f1748d4d5dffa2dd7b7cae4b0cfbd230a2c5c4
Ian Petersen <ispeters@gmail.com>
2023-10-30 15:11:47 -0700

    Format linux/io_uring_syscall.hpp

--- source/linux/io_uring_syscall.hpp
+++ source/linux/io_uring_syscall.hpp
@@ -18,9 +18,14 @@
 #include <signal.h>
 #include UNIFEX_LIBURING_HEADER
 
-namespace unifex::linuxos
-{
-    int io_uring_register(int fd, unsigned opcode, const void *arg, unsigned nr_args);
-    int io_uring_setup(unsigned entries, struct io_uring_params *p);
-    int io_uring_enter(int fd, unsigned to_submit, unsigned min_complete, unsigned flags, sigset_t *sig);
-}
+namespace unifex::linuxos {
+int io_uring_register(
+    int fd, unsigned opcode, const void* arg, unsigned nr_args);
+int io_uring_setup(unsigned entries, struct io_uring_params* p);
+int io_uring_enter(
+    int fd,
+    unsigned to_submit,
+    unsigned min_complete,
+    unsigned flags,
+    sigset_t* sig);
+}  // namespace unifex::linuxos
