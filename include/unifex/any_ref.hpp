cde78e440e20c23564a02155412c442b665b9dd9
Ian Petersen <ispeters@gmail.com>
2023-10-30 11:03:51 -0700

    Format any_ref.hpp

--- include/unifex/any_ref.hpp
+++ include/unifex/any_ref.hpp
@@ -15,13 +15,13 @@
  */
 #pragma once
 
-#include <unifex/detail/vtable.hpp>
-#include <unifex/detail/with_type_erased_tag_invoke.hpp>
 #include <unifex/std_concepts.hpp>
 #include <unifex/swap.hpp>
+#include <unifex/detail/vtable.hpp>
+#include <unifex/detail/with_type_erased_tag_invoke.hpp>
 
-#include <utility>
 #include <type_traits>
+#include <utility>
 
 #include <unifex/detail/prologue.hpp>
 
@@ -35,19 +35,18 @@ struct _any_ref {
 template <typename... CPOs>
 class _any_ref<CPOs...>::type
   : private with_type_erased_tag_invoke<type, CPOs>... {
-
   // Inline up to 3 vtable-entries. Making a max size of 4 pointers.
   using vtable_holder_t = std::conditional_t<
-    (sizeof...(CPOs) <= 3),
-    detail::inline_vtable_holder<CPOs...>,
-    detail::indirect_vtable_holder<CPOs...>>;
+      (sizeof...(CPOs) <= 3),
+      detail::inline_vtable_holder<CPOs...>,
+      detail::indirect_vtable_holder<CPOs...>>;
 
- public:
-  template (typename Concrete)
-    (requires (!same_as<Concrete const, type const>))
-  /*implicit*/ type(Concrete& impl) noexcept
+public:
+  template(typename Concrete)                           //
+      (requires(!same_as<Concrete const, type const>))  //
+      /*implicit*/ type(Concrete& impl) noexcept
     : vtable_(vtable_holder_t::template create<Concrete>())
-    , impl_((void*) std::addressof(impl)) {}
+    , impl_((void*)std::addressof(impl)) {}
 
   void swap(type& other) noexcept {
     unifex::swap(vtable_, other.vtable_);
@@ -58,10 +57,10 @@ class _any_ref<CPOs...>::type
   //
   // TODO: This is not entirely accurate as it might be possible to have two
   // any_ref objects constructed from the same object at the same address but
-  // with different static-types (e.g. one any_ref is constructed from a base-class)
-  // and thus they would have different vtables. However, we can't just compare
-  // vtables since it's not guaranteed that function pointers will point to the
-  // same address.
+  // with different static-types (e.g. one any_ref is constructed from a
+  // base-class) and thus they would have different vtables. However, we can't
+  // just compare vtables since it's not guaranteed that function pointers will
+  // point to the same address.
   friend bool operator==(type const& left, type const& right) noexcept {
     return left.impl_ == right.impl_;
   }
@@ -70,10 +69,8 @@ class _any_ref<CPOs...>::type
     return !(left == right);
   }
 
- private:
-  friend void swap(type& left, type& right) noexcept {
-    left.swap(right);
-  }
+private:
+  friend void swap(type& left, type& right) noexcept { left.swap(right); }
 
   friend const vtable_holder_t& get_vtable(const type& self) noexcept {
     return self.vtable_;
@@ -89,16 +86,16 @@ class _any_ref<CPOs...>::type
 
 // any_ref is a type-erased wrapper that has reference-semantics.
 //
-// It holds a type-erased reference to a concrete object and allows you to invoke CPOs on that
-// object 
+// It holds a type-erased reference to a concrete object and allows you to
+// invoke CPOs on that object
 //
 // Copying an any_ref just copies the reference, not the underlying object.
-template<typename... CPOs>
+template <typename... CPOs>
 using any_ref = typename _any_ref<CPOs...>::type;
 
-template<auto&... CPOs>
+template <auto&... CPOs>
 using any_ref_t = any_ref<tag_t<CPOs>...>;
 
-} // namespace unifex
+}  // namespace unifex
 
 #include <unifex/detail/epilogue.hpp>
