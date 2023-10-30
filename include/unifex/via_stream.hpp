fa0822d6e3bae866bd46b8d774bdca46b822084f
Ian Petersen <ispeters@gmail.com>
2023-10-30 14:35:09 -0700

    Format via_stream.hpp

--- include/unifex/via_stream.hpp
+++ include/unifex/via_stream.hpp
@@ -23,38 +23,40 @@
 
 namespace unifex {
 namespace _via_stream {
-  struct _fn {
-    template (typename StreamSender, typename Scheduler)
-        (requires scheduler<Scheduler>)
-    auto operator()(Scheduler&& scheduler, StreamSender&& stream) const {
-      return adapt_stream(
-          (StreamSender &&) stream,
-          [s = (Scheduler &&) scheduler](auto&& sender) mutable {
-            return via((decltype(sender))sender, s);
-          });
-    }
-    template (typename StreamSender, typename Scheduler)
-        (requires scheduler<Scheduler>)
-    auto operator()(StreamSender&& stream, Scheduler&& scheduler) const {
-      return adapt_stream(
-          (StreamSender &&) stream,
-          [s = (Scheduler &&) scheduler](auto&& sender) mutable {
-            return via((decltype(sender))sender, s);
-          });
-    }
-    template(typename Scheduler)
-        (requires scheduler<Scheduler>)
-    constexpr auto operator()(Scheduler&& scheduler) const
-        noexcept(is_nothrow_callable_v<
-          tag_t<bind_back>, _fn, Scheduler>)
-        -> bind_back_result_t<_fn, Scheduler> {
-      return bind_back(*this, (Scheduler&&)scheduler);
-    }
-  };
-} // namespace _via_stream
+struct _fn {
+  template(typename StreamSender, typename Scheduler)  //
+      (requires scheduler<Scheduler>)                  //
+      auto
+      operator()(Scheduler&& scheduler, StreamSender&& stream) const {
+    return adapt_stream(
+        (StreamSender &&) stream,
+        [s = (Scheduler &&) scheduler](auto&& sender) mutable {
+          return via((decltype(sender))sender, s);
+        });
+  }
+  template(typename StreamSender, typename Scheduler)  //
+      (requires scheduler<Scheduler>)                  //
+      auto
+      operator()(StreamSender&& stream, Scheduler&& scheduler) const {
+    return adapt_stream(
+        (StreamSender &&) stream,
+        [s = (Scheduler &&) scheduler](auto&& sender) mutable {
+          return via((decltype(sender))sender, s);
+        });
+  }
+  template(typename Scheduler)         //
+      (requires scheduler<Scheduler>)  //
+      constexpr auto
+      operator()(Scheduler&& scheduler) const
+      noexcept(is_nothrow_callable_v<tag_t<bind_back>, _fn, Scheduler>)
+          -> bind_back_result_t<_fn, Scheduler> {
+    return bind_back(*this, (Scheduler &&) scheduler);
+  }
+};
+}  // namespace _via_stream
 
-inline constexpr _via_stream::_fn via_stream {};
+inline constexpr _via_stream::_fn via_stream{};
 
-} // namespace unifex
+}  // namespace unifex
 
 #include <unifex/detail/epilogue.hpp>
