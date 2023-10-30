e2fa9816f1c5eddc1541e80b06c22e5a1fe004ed
Ian Petersen <ispeters@gmail.com>
2023-10-30 14:18:10 -0700

    Format swap.hpp

--- include/unifex/swap.hpp
+++ include/unifex/swap.hpp
@@ -16,8 +16,8 @@
 #pragma once
 
 #include <unifex/config.hpp>
-#include <unifex/detail/concept_macros.hpp>
 #include <unifex/type_traits.hpp>
+#include <unifex/detail/concept_macros.hpp>
 
 #include <unifex/detail/prologue.hpp>
 
@@ -30,55 +30,61 @@ template <typename T, typename U, typename = void>
 extern bool const is_nothrow_swappable_with_v;
 
 namespace _swap {
-    void swap();
+void swap();
 
-    template <typename T, typename U>
-    decltype(static_cast<void>(swap(UNIFEX_DECLVAL(T&&), UNIFEX_DECLVAL(U&&))), std::true_type{}) _try_adl_swap(int);
+template <typename T, typename U>
+decltype(
+    static_cast<void>(swap(UNIFEX_DECLVAL(T &&), UNIFEX_DECLVAL(U&&))),
+    std::true_type{}) _try_adl_swap(int);
 
-    template <typename T, typename U>
-    std::false_type _try_adl_swap(long);
+template <typename T, typename U>
+std::false_type _try_adl_swap(long);
 
-    template <typename T, typename U = T>
-    inline constexpr bool is_adl_swappable_v = decltype(_swap::_try_adl_swap<T, U>(0))::value;
+template <typename T, typename U = T>
+inline constexpr bool is_adl_swappable_v =
+    decltype(_swap::_try_adl_swap<T, U>(0))::value;
 
-    struct _fn {
-        // Dispatch to user-defined swap found via ADL:
-        UNIFEX_TEMPLATE(typename T, typename U)
-            (requires is_adl_swappable_v<T, U>)
-        constexpr void operator()(T &&t, U &&u) const
-            noexcept(noexcept(swap((T &&) t, (U &&) u))) {
-            swap((T &&) t, (U &&) u);
-        }
+struct _fn {
+  // Dispatch to user-defined swap found via ADL:
+  template(typename T, typename U)         //
+      (requires is_adl_swappable_v<T, U>)  //
+      constexpr void
+      operator()(T&& t, U&& u) const
+      noexcept(noexcept(swap((T &&) t, (U &&) u))) {
+    swap((T &&) t, (U &&) u);
+  }
 
-        // For intrinsically swappable (i.e., movable) types for which
-        // a swap overload cannot be found via ADL, swap by moving.
-        UNIFEX_TEMPLATE(typename T)
-            (requires (!is_adl_swappable_v<T &>))
-        constexpr auto operator()(T &a, T &b) const
-            noexcept(noexcept(a = T(static_cast<T&&>(b)))) ->
-            decltype(static_cast<void>(a = T(static_cast<T&&>(b)))) {
-            T tmp = static_cast<T &&>(a);
-            a = static_cast<T &&>(b);
-            b = static_cast<T &&>(tmp);
-        }
+  // For intrinsically swappable (i.e., movable) types for which
+  // a swap overload cannot be found via ADL, swap by moving.
+  template(typename T)                     //
+      (requires(!is_adl_swappable_v<T&>))  //
+      constexpr auto
+      operator()(T& a, T& b) const
+      noexcept(noexcept(a = T(static_cast<T&&>(b))))
+          -> decltype(static_cast<void>(a = T(static_cast<T&&>(b)))) {
+    T tmp = static_cast<T&&>(a);
+    a = static_cast<T&&>(b);
+    b = static_cast<T&&>(tmp);
+  }
 
-        // For arrays of intrinsically swappable (i.e., movable) types
-        // for which a swap overload cannot be found via ADL, swap array
-        // elements by moving.
-        UNIFEX_TEMPLATE(typename T, typename U, std::size_t N)
-            (requires (!is_adl_swappable_v<T (&)[N], U (&)[N]>) &&
-                is_swappable_with_v<T &, U &>)
-        constexpr void operator()(T (&t)[N], U (&u)[N]) const
-            noexcept(is_nothrow_swappable_with_v<T &, U &>) {
-            for(std::size_t i = 0; i < N; ++i)
-                (*this)(t[i], u[i]);
-        }
-    };
-} // namespace _swap
+  // For arrays of intrinsically swappable (i.e., movable) types
+  // for which a swap overload cannot be found via ADL, swap array
+  // elements by moving.
+  template(typename T, typename U, std::size_t N)  //
+      (requires(!is_adl_swappable_v<T (&)[N], U (&)[N]>) &&
+       is_swappable_with_v<T&, U&>)  //
+      constexpr void
+      operator()(T (&t)[N], U (&u)[N]) const
+      noexcept(is_nothrow_swappable_with_v<T&, U&>) {
+    for (std::size_t i = 0; i < N; ++i)
+      (*this)(t[i], u[i]);
+  }
+};
+}  // namespace _swap
 
 namespace _swap_cpo {
-inline constexpr _swap::_fn swap {};
-} // namespace _swap_cpo
+inline constexpr _swap::_fn swap{};
+}  // namespace _swap_cpo
 using namespace _swap_cpo;
 
 template <typename, typename, typename>
@@ -86,15 +92,19 @@ inline constexpr bool is_swappable_with_v = false;
 
 template <typename T, typename U>
 inline constexpr bool is_swappable_with_v<
-    T, U, decltype(unifex::swap(UNIFEX_DECLVAL(T&&), UNIFEX_DECLVAL(U&&)))> = true;
+    T,
+    U,
+    decltype(unifex::swap(UNIFEX_DECLVAL(T &&), UNIFEX_DECLVAL(U&&)))> = true;
 
 template <typename, typename, typename>
 inline constexpr bool is_nothrow_swappable_with_v = false;
 
 template <typename T, typename U>
 inline constexpr bool is_nothrow_swappable_with_v<
-    T, U, decltype(unifex::swap(UNIFEX_DECLVAL(T&&), UNIFEX_DECLVAL(U&&)))> = 
+    T,
+    U,
+    decltype(unifex::swap(UNIFEX_DECLVAL(T &&), UNIFEX_DECLVAL(U&&)))> =
     noexcept(swap(UNIFEX_DECLVAL(T&&), UNIFEX_DECLVAL(U&&)));
-} // namespace unifex
+}  // namespace unifex
 
 #include <unifex/detail/epilogue.hpp>
