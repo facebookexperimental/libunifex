9538725108746645cbdd82afde3eabb104644720
Deniz Evrenci <denizevrenci@gmail.com>
2024-04-15 15:42:21 -0700

    Remove the cmake configuration machinery for config.hpp
    We can rely on the preprocessor for it instead since we only support
    C++17 and above.

--- cmake/unifex_flags.cmake
+++ cmake/unifex_flags.cmake
@@ -9,28 +9,6 @@ include(CMakePushCheckState)
 include(CheckIncludeFile)
 include(CheckSymbolExists)
 
-# Probe for coroutine TS support
-find_package(Coroutines COMPONENTS Experimental Final)
-# MSVC's coroutines support is too broken to support here, sadly.
-if(CXX_COROUTINES_HAVE_COROUTINES AND NOT UNIFEX_CXX_COMPILER_MSVC)
-  set(UNIFEX_NO_COROUTINES FALSE)
-  set(UNIFEX_COROUTINES_HEADER ${CXX_COROUTINES_HEADER})
-  set(UNIFEX_COROUTINES_NAMESPACE ${CXX_COROUTINES_NAMESPACE})
-else()
-  set(UNIFEX_NO_COROUTINES TRUE)
-endif()
-
-# Probe for memory_resource support
-find_package(MemoryResource COMPONENTS Experimental Final)
-# Set some variables to be used by configure_file.
-if(CXX_MEMORY_RESOURCE_HAVE_PMR)
-  set(UNIFEX_NO_MEMORY_RESOURCE FALSE)
-  set(UNIFEX_MEMORY_RESOURCE_HEADER ${CXX_MEMORY_RESOURCE_HEADER})
-  set(UNIFEX_MEMORY_RESOURCE_NAMESPACE ${CXX_MEMORY_RESOURCE_NAMESPACE})
-else()
-  set(UNIFEX_NO_MEMORY_RESOURCE TRUE)
-endif()
-
 if(DEFINED UNIFEX_NO_LIBURING)
   message(WARNING "[unifex warning]: forcing no_liburing=${UNIFEX_NO_LIBURING} !")
 elseif(DEFINED ENV{UNIFEX_NO_LIBURING})
