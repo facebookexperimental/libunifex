471e9c4e5a13f29407c81f114c51b20db486a416
Max Motovilov <mmotovilov@fb.com>
2025-10-30 14:11:07 -0700

    [fix] timing tweaks for mac os gcc CI runner

--- test/async_mutex_v2_test.cpp
+++ test/async_mutex_v2_test.cpp
@@ -19,15 +19,15 @@
 #if !UNIFEX_NO_COROUTINES
 
 #  include <unifex/async_manual_reset_event.hpp>
-#  include <unifex/v2/async_mutex.hpp>
 #  include <unifex/let_done.hpp>
 #  include <unifex/scheduler_concepts.hpp>
 #  include <unifex/scope_guard.hpp>
 #  include <unifex/single_thread_context.hpp>
-#  include <unifex/timed_single_thread_context.hpp>
 #  include <unifex/stop_when.hpp>
 #  include <unifex/sync_wait.hpp>
 #  include <unifex/task.hpp>
+#  include <unifex/timed_single_thread_context.hpp>
+#  include <unifex/v2/async_mutex.hpp>
 #  include <unifex/when_all.hpp>
 
 #  include <gtest/gtest.h>
@@ -68,8 +68,7 @@ TEST(async_mutex_v2, multiple_threads) {
 using namespace std::chrono_literals;
 
 class async_mutex_v2_test : public ::testing::Test {
- public:
-
+public:
   static auto timer() noexcept {
     static timed_single_thread_context timer;
     return timer.get_scheduler();
@@ -77,21 +76,19 @@ class async_mutex_v2_test : public ::testing::Test {
 
   static auto maybeCancelled(bool& cancelled) {
     cancelled = false;
-    return let_done([&cancelled]() { 
+    return let_done([&cancelled]() {
       cancelled = true;
-      return just(); 
+      return just();
     });
-  }  
+  }
 
   task<void> critSecTask(bool& acquired, bool& finished) {
     acquired = false;
     finished = false;
     co_await mutex.async_lock();
     acquired = true;
-    scope_guard guard([this]() noexcept {
-      mutex.unlock(); 
-    });
-    co_await schedule_after(timer(), 200ms);
+    scope_guard guard([this]() noexcept { mutex.unlock(); });
+    co_await schedule_after(timer(), 500ms);
     finished = true;
   }
 
@@ -99,25 +96,25 @@ class async_mutex_v2_test : public ::testing::Test {
     acquired = false;
     finished = false;
     bool cancelled = false;
-    co_await (schedule_after(timer(), 200ms) |
-              maybeCancelled(cancelled));
+    co_await (schedule_after(timer(), 200ms) | maybeCancelled(cancelled));
     co_await critSecTask(acquired, finished);
-    EXPECT_TRUE(!cancelled || !acquired); // cancelled implies !acquired
-  }  
+    EXPECT_TRUE(!cancelled || !acquired);  // cancelled implies !acquired
+  }
 
   async_mutex mutex;
 };
 
 TEST_F(async_mutex_v2_test, cancel_awaiting) {
-  bool first_acquired, first_finished, second_acquired,
-       second_finished, second_cancelled;
+  bool first_acquired, first_finished, second_acquired, second_finished,
+      second_cancelled;
 
   sync_wait(when_all(
-    critSecTask(first_acquired, first_finished),
-    stop_when(
-      critSecTask(second_acquired, second_finished) | maybeCancelled(second_cancelled),
-      schedule_after(timer(), 100ms))));
-  
+      critSecTask(first_acquired, first_finished),
+      stop_when(
+          critSecTask(second_acquired, second_finished) |
+              maybeCancelled(second_cancelled),
+          schedule_after(timer(), 100ms))));
+
   EXPECT_TRUE(first_acquired);
   EXPECT_TRUE(first_finished);
   EXPECT_TRUE(second_cancelled);
@@ -126,15 +123,16 @@ TEST_F(async_mutex_v2_test, cancel_awaiting) {
 }
 
 TEST_F(async_mutex_v2_test, cancel_running) {
-  bool first_acquired, first_finished, first_cancelled,
-       second_acquired, second_finished;
+  bool first_acquired, first_finished, first_cancelled, second_acquired,
+      second_finished;
 
   sync_wait(when_all(
-    stop_when(
-      critSecTask(first_acquired, first_finished) | maybeCancelled(first_cancelled),
-      schedule_after(timer(), 100ms)),
-    critSecTask(second_acquired, second_finished)));
-  
+      stop_when(
+          critSecTask(first_acquired, first_finished) |
+              maybeCancelled(first_cancelled),
+          schedule_after(timer(), 100ms)),
+      critSecTask(second_acquired, second_finished)));
+
   EXPECT_TRUE(first_acquired);
   EXPECT_FALSE(first_finished);
   EXPECT_TRUE(first_cancelled);
@@ -143,17 +141,17 @@ TEST_F(async_mutex_v2_test, cancel_running) {
 }
 
 TEST_F(async_mutex_v2_test, cancel_awaiting_middle) {
-  bool first_acquired, first_finished, second_acquired,
-       second_finished, second_cancelled, third_acquired,
-       third_finished;
+  bool first_acquired, first_finished, second_acquired, second_finished,
+      second_cancelled, third_acquired, third_finished;
 
   sync_wait(when_all(
-    critSecTask(first_acquired, first_finished),
-    stop_when(
-      critSecTask(second_acquired, second_finished) | maybeCancelled(second_cancelled),
-      schedule_after(timer(), 100ms)),
-    critSecTask(third_acquired, third_finished)));
-  
+      critSecTask(first_acquired, first_finished),
+      stop_when(
+          critSecTask(second_acquired, second_finished) |
+              maybeCancelled(second_cancelled),
+          schedule_after(timer(), 100ms)),
+      critSecTask(third_acquired, third_finished)));
+
   EXPECT_TRUE(first_acquired);
   EXPECT_TRUE(first_finished);
   EXPECT_TRUE(second_cancelled);
@@ -167,24 +165,26 @@ TEST_F(async_mutex_v2_test, cancel_early) {
   bool first_acquired, first_finished, first_cancelled;
 
   sync_wait(stop_when(
-      delayedCritSecTask(first_acquired, first_finished) | maybeCancelled(first_cancelled),
+      delayedCritSecTask(first_acquired, first_finished) |
+          maybeCancelled(first_cancelled),
       schedule_after(timer(), 100ms)));
-  
+
   EXPECT_FALSE(first_acquired);
   EXPECT_FALSE(first_finished);
   EXPECT_TRUE(first_cancelled);
 }
 
 TEST_F(async_mutex_v2_test, cancel_awaiting_early) {
-  bool first_acquired, first_finished, second_acquired,
-       second_finished, second_cancelled;
+  bool first_acquired, first_finished, second_acquired, second_finished,
+      second_cancelled;
 
   sync_wait(when_all(
-    critSecTask(first_acquired, first_finished),
-    stop_when(
-      delayedCritSecTask(second_acquired, second_finished) | maybeCancelled(second_cancelled),
-      schedule_after(timer(), 100ms))));
-  
+      critSecTask(first_acquired, first_finished),
+      stop_when(
+          delayedCritSecTask(second_acquired, second_finished) |
+              maybeCancelled(second_cancelled),
+          schedule_after(timer(), 100ms))));
+
   EXPECT_TRUE(first_acquired);
   EXPECT_TRUE(first_finished);
   EXPECT_TRUE(second_cancelled);
