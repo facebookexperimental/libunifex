4131b4b295551d7606fe2dee606fbf3ea887fb9b
Max Motovilov <mmotovilov@fb.com>
2025-11-18 17:50:57 -0800

    Added running commentary to the example code

--- examples/async_pass.cpp
+++ examples/async_pass.cpp
@@ -44,6 +44,12 @@ public:
 
   auto shutdown() { return scope_.cleanup(); }
 
+  // This is the simulated API intended to process UI event "Call". It is
+  // synchronous and may be called repeatedly at an unpredictable rate, faster
+  // than the underlying service can process.
+  // The API guarantees that any new call will hang up an active one if any, and
+  // replace any previously placed but not yet started call w/o letting the
+  // service see it to avoid thrashing.
   void placeCall(std::string_view to);
 
 private:
@@ -63,9 +69,16 @@ task<void> CallingService::service() {
   static constexpr auto kHangupDuration{100ms};
 
   for (;;) {
+    // Service will only accept a new call when the previous one has been
+    // finished and cleanup completed. The agentCallRequest_ provides an
+    // essential guarantee to the agent() that the service has accepted the
+    // call.
     auto to = co_await agentCallRequest_.async_accept();
     std::cout << "Calling " << to << std::endl;
     co_await when_any(
+        // The hangupRequest_ could be a simple event, but the essential
+        // requirement is that it is cancellable by normal call completion.
+        // Unfortunately the manual_reset_async_event does not satisfy it.
         hangupRequest_.async_accept() | let_value([&to]() {
           std::cout << "Hung up on " << to << std::endl;
           return schedule_after(timer.get_scheduler(), kHangupDuration);
@@ -79,7 +92,14 @@ task<void> CallingService::service() {
 task<void> CallingService::agent() {
   std::string callee;
   for (;;) {
+    // On each iteration of the loop, either
+    // (1) callee is set to the value obtained from userCallRequest_ (possibly
+    // replacing an old, unused, one), or
+    // (2) callee value is successfully accepted by the service() and callee is
+    // reset
     auto userRequest =
+        // The userCallRequest_ can be replaced with a queue with placeCall()
+        // serving as producer. This will not break the loop invariant.
         userCallRequest_.async_accept() | then([&callee](auto to) {
           if (auto previous = std::exchange(callee, std::move(to));
               !previous.empty()) {
@@ -105,18 +125,26 @@ void CallingService::placeCall(std::string_view to) {
     std::cout << "Hanging up previously placed call" << std::endl;
   }
 
-  // Can be done with a detached task instead. Agent responds immediately so
-  // the sync_wait is for the context switches only.
+  // Agent responds immediately so sync_wait() blocks only for a short time
+  // necessary to switch contexts. A queue can serve the same purpose in place
+  // of async_pass.
   sync_wait(userCallRequest_.async_call(std::string{to}));
 }
 
 task<void> callClient(CallingService& service) {
   using namespace std::chrono_literals;
   service.placeCall("Alice");
+  // cout> Trying to call Alice
   co_await schedule_after(timer.get_scheduler(), 300ms);
+  // cout> Calling Alice
   service.placeCall("Bob");
+  // cout> Trying to call Bob
+  // cout> Hanging up previously placed call
   service.placeCall("Charlie");
+  // cout> Trying to call Charlie
+  // cout> Cancelled earlier call to Bob
   co_await schedule_after(timer.get_scheduler(), 600ms);
+  // cout> Calling Charlie
 }
 
 }  // namespace
