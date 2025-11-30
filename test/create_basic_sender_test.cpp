8eb0920a0245dfd5ef84743d28f7820da9e23c33
Max Motovilov <mmotovilov@fb.com>
2025-11-30 09:58:52 -0800

    [fix?] gcc 12-13

--- test/create_basic_sender_test.cpp
+++ test/create_basic_sender_test.cpp
@@ -488,9 +488,6 @@ TEST_F(create_basic_sender_test, opaque_callback_exact_type_fallback) {
   EXPECT_EQ(1234, late_result);
 }
 
-#  if defined(__GNUC__) && __GNUC__ < 14
-// Omit this test due to GCC failure to deduce pack size of args...
-#  else
 TEST_F(create_basic_sender_test, opaque_callback_type_erased_fallback) {
   int late_result{0};
   using safe_callback_t =
@@ -521,8 +518,10 @@ TEST_F(create_basic_sender_test, opaque_callback_type_erased_fallback) {
   sync_wait(schedule_after(timer.get_scheduler(), 500ms));
   EXPECT_EQ(1234, late_result);
 }
-#  endif
 
+#  if defined(__GNUC__) && __GNUC__ < 14
+// Omit this test due to GCC failure to deduce pack size of args...
+#  else
 TEST_F(create_basic_sender_test, opaque_callback_ptr_fallback) {
   using safe_callback_t =
       basic_sender_opaque_callback_with_fallback<void (*)(int), int>;
@@ -551,6 +550,7 @@ TEST_F(create_basic_sender_test, opaque_callback_ptr_fallback) {
 
   sync_wait(schedule_after(timer.get_scheduler(), 500ms));
 }
+#  endif
 
 TEST_F(create_basic_sender_test, non_affine_set_value) {
   auto threadId{std::this_thread::get_id()};
