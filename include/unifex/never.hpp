39f371daf3c9ad21341d6bd4e0ad1f19e617f17a
Max Motovilov <mmotovilov@fb.com>
2026-02-22 19:02:23 -0800

    Added just_void_or_never() based on extended never_sender

--- include/unifex/never.hpp
+++ include/unifex/never.hpp
@@ -30,15 +30,19 @@
 namespace unifex {
 namespace _never {
 
-template <typename Receiver>
+template <bool CanSendVoid>
+using maybe_bool = std::conditional_t<CanSendVoid, bool, std::in_place_t>;
+
+template <typename Receiver, bool CanSendVoid>
 struct _op {
   struct type;
 };
-template <typename Receiver>
-using operation = typename _op<remove_cvref_t<Receiver>>::type;
 
-template <typename Receiver>
-struct _op<Receiver>::type {
+template <typename Receiver, bool CanSendVoid>
+using operation = typename _op<remove_cvref_t<Receiver>, CanSendVoid>::type;
+
+template <typename Receiver, bool CanSendVoid>
+struct _op<Receiver, CanSendVoid>::type {
   struct cancel_callback {
     type& op_;
     void operator()() noexcept {
@@ -50,40 +54,60 @@ struct _op<Receiver>::type {
   using stop_token_type = stop_token_type_t<Receiver&>;
 
   static_assert(
-      !is_stop_never_possible_v<stop_token_type>,
+      CanSendVoid || !is_stop_never_possible_v<stop_token_type>,
       "never should not be used with a stop-token "
       "type that can never be stopped.");
-
   UNIFEX_NO_UNIQUE_ADDRESS Receiver receiver_;
   manual_lifetime<
       typename stop_token_type::template callback_type<cancel_callback>>
       stopCallback_;
+  UNIFEX_NO_UNIQUE_ADDRESS maybe_bool<CanSendVoid> isVoid_;
 
   template(typename Receiver2)                            //
       (requires constructible_from<Receiver, Receiver2>)  //
-      type(Receiver2&& receiver)
-    : receiver_((Receiver2 &&) receiver) {}
+      type(Receiver2&& receiver, maybe_bool<CanSendVoid> isVoid) noexcept(
+          std::is_nothrow_constructible_v<Receiver, Receiver2&&>)
+    : receiver_((Receiver2&&)receiver)
+    , isVoid_(isVoid) {}
 
-  void start() noexcept {
+  void start() & noexcept {
+    if constexpr (CanSendVoid) {
+      if (isVoid_) {
+        unifex::set_value((Receiver&&)receiver_);
+        return;
+      }
+    }
+
     UNIFEX_ASSERT(get_stop_token(receiver_).stop_possible());
     stopCallback_.construct(get_stop_token(receiver_), cancel_callback{*this});
   }
 };
 
+template <bool CanSendVoid>
 struct sender {
+  maybe_bool<CanSendVoid> isVoid_{};
+
+  sender() = default;
+  explicit constexpr sender(bool isVoid) noexcept {
+    if constexpr (CanSendVoid) {
+      isVoid_ = isVoid;
+    }
+  }
+
   template <
-      template <typename...>
-      class Variant,
-      template <typename...>
-      class Tuple>
-  using value_types = Variant<>;
+      template <typename...> class Variant,
+      template <typename...> class Tuple>
+  using value_types =
+      std::conditional_t<CanSendVoid, Variant<Tuple<>>, Variant<>>;
 
   template <template <typename...> class Variant>
-  using error_types = Variant<>;
+  using error_types =
+      std::conditional_t<CanSendVoid, Variant<std::exception_ptr>, Variant<>>;
 
   static constexpr bool sends_done = true;
 
-  // we'll complete inline if started with a stopped stop token
+  // we'll complete inline if started with a stopped stop token or if isVoid_
+  // is true
   static constexpr blocking_kind blocking = blocking_kind::maybe;
 
   // we complete wherever the stop callback is invoked, which must be on the
@@ -91,13 +115,15 @@ struct sender {
   static constexpr bool is_always_scheduler_affine = true;
 
   template <typename Receiver>
-  operation<Receiver> connect(Receiver&& receiver) const {
-    return operation<Receiver>{(Receiver &&) receiver};
+  operation<Receiver, CanSendVoid> connect(Receiver&& receiver) const {
+    return operation<Receiver, CanSendVoid>{(Receiver&&)receiver, isVoid_};
   }
 };
 
+explicit sender(bool) -> sender<true>;
+
 struct stream {
-  friend constexpr sender tag_invoke(tag_t<next>, stream&) noexcept {
+  friend constexpr sender<false> tag_invoke(tag_t<next>, stream&) noexcept {
     return {};
   }
   friend constexpr auto tag_invoke(tag_t<cleanup>, stream&) noexcept {
@@ -106,7 +132,7 @@ struct stream {
 };
 }  // namespace _never
 
-using never_sender = _never::sender;
+using never_sender = _never::sender<false>;
 using never_stream = _never::stream;
 }  // namespace unifex
 
