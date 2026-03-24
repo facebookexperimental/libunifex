1d9d8a075c64df5b163c05002e561b4539cab76b
...Max... <max@motovilov.com>
2026-03-24 09:31:49 -0500

    Revert "[fix] race between external callback and opstate destructor + technical UB"
    This reverts commit ba145dce2b2307843c30a37bec3bd03ccb7c3f0c.

--- include/unifex/create_basic_sender.hpp
+++ include/unifex/create_basic_sender.hpp
@@ -298,7 +298,7 @@ struct _safe_cb_base {
   struct ptr {
     template <typename Op>
     Op* op() const noexcept {
-      return static_cast<Op*>(*ptr_);
+      return *std::reinterpret_pointer_cast<Op*>(ptr_);
     }
 
     operator bool() const noexcept { return ptr_ != nullptr; }
@@ -541,9 +541,6 @@ struct _stop_callback {
       }
 
       completed = op_.state_.completed();
-      if (completed) {
-        op_.safe_cb_holder_.reset();
-      }
     }
 
     if (completed) {
@@ -754,7 +751,6 @@ private:
       UNIFEX_CATCH(...) {
         auto guard{self.lock()};
         self.set_error(std::current_exception());
-        self.safe_cb_holder_.reset();
         completed = true;
       }
     }
@@ -780,11 +776,7 @@ private:
       body(_event<_event_type::start>{});
     }
 
-    auto completed = state_.completed();
-    if (completed) {
-      safe_cb_holder_.reset();
-    }
-    return completed;
+    return state_.completed();
   }
 
   template <bool Noexcept, typename Event, typename... Args>
@@ -809,9 +801,6 @@ private:
       }
 
       completed = state_.completed();
-      if (completed) {
-        safe_cb_holder_.reset();
-      }
     }
 
     if (completed) {
@@ -835,12 +824,8 @@ private:
     return safe_cb_holder_;
   }
 
-  state_t state_;
-  // Declared after state_ so it is destroyed first. Also explicitly reset
-  // under the lock in every completion path, ensuring that safe callbacks
-  // arriving after the sender has completed see an expired weak_ptr and
-  // never touch the (possibly destroyed) operation state.
   _safe_cb_base::holder safe_cb_holder_{nullptr};
+  state_t state_;
 
   UNIFEX_NO_UNIQUE_ADDRESS receiver_wrapper_t receiver_;
   UNIFEX_NO_UNIQUE_ADDRESS stop_callback_t stop_;
